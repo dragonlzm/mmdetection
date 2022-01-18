b8fbe83f52cc40a74595b5374bb25de0f7f5f5ff
dragonlzm <liuzhuom@usc.edu>
2022-01-18 15:03:58 -0800

    update for 345 246 config

--- configs/new_rpn/new_rpn_patches345_coco.py
+++ configs/new_rpn/new_rpn_patches345_coco.py
@@ -6,7 +6,7 @@ model = dict(
     rpn_head=dict(patches_list=[3, 4, 5]))
 
 runner = dict(type='EpochBasedRunner', max_epochs=24)
-lr_config = dict(_delete_=True, policy='step', step=[16, 22])
+lr_config = dict(_delete_=True, policy='step', step=[12, 22])
 
 # for 2 gpus training
 optimizer = dict(lr=0.00005)
