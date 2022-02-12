24204d23c3945556e15e89ace1adf8f7cef0770a
dragonlzm <liuzhuom@usc.edu>
2022-02-11 23:58:31 -0800

    update for use

--- visualize_all_bbox_for_one_image.py
+++ visualize_all_bbox_for_one_image.py
@@ -10,7 +10,10 @@ import matplotlib.patches as patches
 from PIL import Image
 
 #save_path = 'C:\\Users\\XPS\\Desktop\\174228.jpg'
-save_path = 'C:\\Users\\XPS\\Desktop\\472054.jpg'
+#save_path = 'C:\\Users\\XPS\\Desktop\\472054.jpg'
+#save_path = 'C:\\Users\\XPS\\Desktop\\397133.jpg'
+save_path = 'C:\\Users\\XPS\\Desktop\\174482.jpg'
+
 
 #save_path = 'C:\\Users\\XPS\\Desktop\\gt_bbox\\large_person\\000000554002.jpg'
 #save_path = 'C:\\Users\\XPS\\Desktop\\random_bbox\\000000507037.jpg'
@@ -33,8 +36,8 @@ for ratio in proposal_result:
     bbox = [x, y, w, h]
     all_bbox.append(bbox)'''
 
-#gt_annotation_path = "C:\\Users\\XPS\\Desktop\\annotations\\instances_val2017.json"
-gt_annotation_path = "C:\\Users\\XPS\\Desktop\\annotations\\instances_train2017.json"
+gt_annotation_path = "C:\\Users\\XPS\\Desktop\\annotations\\instances_val2017.json"
+#gt_annotation_path = "C:\\Users\\XPS\\Desktop\\annotations\\instances_train2017.json"
 gt_anno_result = json.load(open(gt_annotation_path))
 
 from_img_id_to_gt = {}
@@ -62,7 +65,9 @@ for ele in gt_anno_result['annotations']:
 #save_root = 'C:\\Users\\XPS\\Desktop\\gt_bbox\\000000507037\\'
 #save_root = 'C:\\Users\\XPS\\Desktop\\gt_bbox\\000000554002\\'
 #save_root = 'C:\\Users\\XPS\\Desktop\\gt_bbox\\000000174228\\'
-save_root = 'C:\\Users\\XPS\\Desktop\\gt_bbox\\000000472054\\'
+#save_root = 'C:\\Users\\XPS\\Desktop\\gt_bbox\\000000472054\\'
+#save_root = 'C:\\Users\\XPS\\Desktop\\gt_bbox\\000000397133\\'
+save_root = 'C:\\Users\\XPS\\Desktop\\gt_bbox\\000000174482\\'
 
 #for i, box in enumerate(all_bbox):
 #for i, box in enumerate(from_img_id_to_gt[275198]):
@@ -73,17 +78,18 @@ save_root = 'C:\\Users\\XPS\\Desktop\\gt_bbox\\000000472054\\'
 #from_img_id_to_gt[174228].append([278.87, 126.33, 170, 216.99])
 #from_img_id_to_gt[174228].append([278.87, 126.33, 190, 216.99])
 #from_img_id_to_gt[174228].append([278.87, 126.33, 210, 216.99])
-from_img_id_to_gt[472054].append([302.25, 90.13, 130.6, 326.66])
-from_img_id_to_gt[472054].append([282.25, 90.13, 150.6, 326.66])
-from_img_id_to_gt[472054].append([262.25, 90.13, 170.6, 326.66])
-from_img_id_to_gt[472054].append([242.25, 90.13, 190.6, 326.66])
-from_img_id_to_gt[472054].append([222.25, 90.13, 210.6, 326.66])
+#from_img_id_to_gt[472054].append([302.25, 90.13, 130.6, 326.66])
+#from_img_id_to_gt[472054].append([282.25, 90.13, 150.6, 326.66])
+#from_img_id_to_gt[472054].append([262.25, 90.13, 170.6, 326.66])
+#from_img_id_to_gt[472054].append([242.25, 90.13, 190.6, 326.66])
+#from_img_id_to_gt[472054].append([222.25, 90.13, 210.6, 326.66])
 
 #[357.29, 126.33, 282.71, 216.99]
 #[278.87, 155.85, 136.1, 199.68]
 #for i, box in enumerate(from_img_id_to_gt[174228]):
-for i, box in enumerate(from_img_id_to_gt[472054]):  
-
+#for i, box in enumerate(from_img_id_to_gt[472054]):  
+#for i, box in enumerate(from_img_id_to_gt[397133]):     
+for i, box in enumerate(from_img_id_to_gt[174482]):
     #box = annotation['bbox']
     fig,ax = plt.subplots(1)
 
@@ -103,6 +109,9 @@ for i, box in enumerate(from_img_id_to_gt[472054]):
     #file_name = '000000275198_' + str(i) + '.jpg' 
     #file_name = '000000554002_' + str(i) + '.jpg' 
     #file_name = '000000174228_' + str(i) + '.jpg'    
-    file_name = '000000472054_' + str(i) + '.jpg'     
+    #file_name = '000000472054_' + str(i) + '.jpg'
+    #file_name = '000000397133_' + str(i) + '.jpg'
+    file_name = '000000174482_' + str(i) + '.jpg'
+         
     plt.savefig(print_path+file_name)
     plt.close()
