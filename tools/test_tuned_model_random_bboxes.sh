8d8629c973ffd5fb8891256fef37cf6e0ddc6764
dragonlzm <liuzhuom@usc.edu>
2022-02-24 21:31:56 -0800

    update for shell script

--- tools/test_tuned_model_random_bboxes.sh
+++ tools/test_tuned_model_random_bboxes.sh
@@ -14,36 +14,35 @@ module load gcc/8.3.0
 #module load cuda/10.1.243
 #./program
 
-cd /home1/liuzhuom/mmdetection
+cd /project/nevatia_174/zhuoming/code/new_rpn/mmdetection
 rm -rf ./data
 ln -sf /project/nevatia_174/zhuoming/detection ./data
 
 # raw
-PYTHONPATH="/home1/liuzhuom/mmdetection":$PYTHONPATH \
+PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=2 \
-    /home1/liuzhuom/mmdetection/tools/test.py \
-    /home1/liuzhuom/mmdetection/configs/cls_finetuner/cls_finetuner_clip_full_coco.py \
+    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/test.py \
+    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/configs/cls_finetuner/cls_finetuner_clip_full_coco.py \
     /home1/liuzhuom/epoch_0.pth \
-    --launcher pytorch --eval=gt_acc --options jsonfile_prefix=/home1/liuzhuom/mmdetection/results \
-    --cfg-options data.test.visualization_path=/home1/liuzhuom/mmdetection/visualization_random_bbox \
-    model.test_cfg.test_with_rand_bboxes=True model.test_cfg.rpn_head.test_with_rand_bboxes=True
+    --launcher pytorch --eval=gt_acc --options jsonfile_prefix=/project/nevatia_174/zhuoming/code/new_rpn/mmdetection/results \
+    --cfg-options model.test_cfg.test_with_rand_bboxes=True model.test_cfg.rpn_head.test_with_rand_bboxes=True data.test.visualization_path=/home1/liuzhuom/mmdetection/visualization_random_bbox
+    
 
 # 48 finetune
-PYTHONPATH="/home1/liuzhuom/mmdetection":$PYTHONPATH \
+PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=2 \
-    /home1/liuzhuom/mmdetection/tools/test.py \
-    /home1/liuzhuom/mmdetection/configs/cls_finetuner/cls_finetuner_clip_full_coco.py \
+    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/test.py \
+    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/configs/cls_finetuner/cls_finetuner_clip_full_coco.py \
     /project/nevatia_174/zhuoming/detection/test/cls_finetuner_clip_base_all_train/latest.pth \
-    --launcher pytorch --eval=gt_acc --options jsonfile_prefix=/home1/liuzhuom/mmdetection/results \
-    --cfg-options data.test.visualization_path=/home1/liuzhuom/mmdetection/visualization_random_bbox \
-    model.test_cfg.test_with_rand_bboxes=True model.test_cfg.rpn_head.test_with_rand_bboxes=True
+    --launcher pytorch --eval=gt_acc --options jsonfile_prefix=/project/nevatia_174/zhuoming/code/new_rpn/mmdetection/results \
+    --cfg-options model.test_cfg.test_with_rand_bboxes=True model.test_cfg.rpn_head.test_with_rand_bboxes=True data.test.visualization_path=/home1/liuzhuom/mmdetection/visualization_random_bbox
+
 
 # full finetune
-PYTHONPATH="/home1/liuzhuom/mmdetection":$PYTHONPATH \
+PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=2 \
-    /home1/liuzhuom/mmdetection/tools/test.py \
-    /home1/liuzhuom/mmdetection/configs/cls_finetuner/cls_finetuner_clip_full_coco.py \
+    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/test.py \
+    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/configs/cls_finetuner/cls_finetuner_clip_full_coco.py \
     /project/nevatia_174/zhuoming/detection/test/cls_finetuner_clip_full_coco/epoch_12.pth \
-    --launcher pytorch --eval=gt_acc --options jsonfile_prefix=/home1/liuzhuom/mmdetection/results \
-    --cfg-options data.test.visualization_path=/home1/liuzhuom/mmdetection/visualization_random_bbox \
-    model.test_cfg.test_with_rand_bboxes=True model.test_cfg.rpn_head.test_with_rand_bboxes=True
+    --launcher pytorch --eval=gt_acc --options jsonfile_prefix=/project/nevatia_174/zhuoming/code/new_rpn/mmdetection/results \
+    --cfg-options model.test_cfg.test_with_rand_bboxes=True model.test_cfg.rpn_head.test_with_rand_bboxes=True data.test.visualization_path=/home1/liuzhuom/mmdetection/visualization_random_bbox
