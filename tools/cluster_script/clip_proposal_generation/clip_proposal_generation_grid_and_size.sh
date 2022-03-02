794bcbf3890e46d080fa986200009ecbe055b47a
dragonlzm <dragonlzm123@gmail.com>
2022-03-01 23:12:15 -0800

    details updated

--- tools/cluster_script/clip_proposal_generation/clip_proposal_generation_grid_and_size.sh
+++ tools/cluster_script/clip_proposal_generation/clip_proposal_generation_grid_and_size.sh
@@ -41,5 +41,5 @@ python -m torch.distributed.launch --nproc_per_node=2 \
     /home1/liuzhuom/mmdetection/tools/test.py \
     /home1/liuzhuom/mmdetection/configs/cls_proposal_generator/cls_proposal_generator_coco.py \
     /project/nevatia_174/zhuoming/detection/test/cls_finetuner_clip_base_all_train/latest.pth \
-    --launcher pytorch --eval=proposal_fast --options jsonfile_prefix=/project/nevatia_174/zhuoming/detection/test/cls_proposal_generator_coco/results_16_32_512 \
+    --launcher pytorch --eval=proposal_fast --options jsonfile_prefix=/project/nevatia_174/zhuoming/detection/test/cls_proposal_generator_coco/results_16_16_1024 \
     --cfg-options model.anchor_generator.strides=[16] model.anchor_generator.scales=[2, 4, 8, 16, 32]
