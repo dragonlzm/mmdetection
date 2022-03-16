3e0a303a4b3054a097b6eefa563afdd006b35e6a
dragonlzm <dragonlzm123@gmail.com>
2022-03-16 14:58:47 -0700

    bug fixed

--- configs/mask_rcnn_distill/mask_rcnn_distill_r50_fpn_1x_coco.py
+++ configs/mask_rcnn_distill/mask_rcnn_distill_r50_fpn_1x_coco.py
@@ -1,6 +1,6 @@
 _base_ = [
     '../_base_/models/mask_rcnn_r50_fpn_distillation.py',
-    '../_base_/datasets/coco_instance_wtih_distillation.py',
+    '../_base_/datasets/coco_instance_with_distillation.py',
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
 
