9f1116fb65d68cdae06cbb989433a2621535bf8b
dragonlzm <dragonlzm123@gmail.com>
2022-03-24 13:44:43 -0700

    update for 2x setting

--- tools/mask_rcnn_r50_fpn_1x_coco_2gpu.sh
+++ tools/mask_rcnn_r50_fpn_1x_coco_2gpu.sh
@@ -15,8 +15,8 @@ module load gcc/8.3.0
 #./program
 
 cd /project/nevatia_174/zhuoming/code/new_rpn/mmdetection
-rm -rf ./data
-ln -sf /project/nevatia_174/zhuoming/detection ./data
+#rm -rf ./data
+#ln -sf /project/nevatia_174/zhuoming/detection ./data
 
 #PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
 #python -m torch.distributed.launch --nproc_per_node=2 \
@@ -25,9 +25,18 @@ ln -sf /project/nevatia_174/zhuoming/detection ./data
 #    --work-dir=/project/nevatia_174/zhuoming/detection/test/mask_rcnn_r50_fpn_1x_coco_2gpu 
     #--resume-from=/project/nevatia_174/zhuoming/detection/test/mask_rcnn_r50_fpn_1x_coco_2gpu/latest.pth
 
+# for 1x training
+#PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+#python -m torch.distributed.launch --nproc_per_node=2 \
+#    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+#    configs/mask_rcnn/mask_rcnn_r50_fpn_1x_coco_2gpu_base48.py --launcher pytorch \
+#    --work-dir=/project/nevatia_174/zhuoming/detection/test/mask_rcnn_r50_fpn_1x_coco_2gpu_base48 \
+#    --resume-from=/project/nevatia_174/zhuoming/detection/test/mask_rcnn_r50_fpn_1x_coco_2gpu_base48/latest.pth
+
+# for 2x training first stage
 PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=2 \
     /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
-    configs/mask_rcnn/mask_rcnn_r50_fpn_1x_coco_2gpu_base48.py --launcher pytorch \
-    --work-dir=/project/nevatia_174/zhuoming/detection/test/mask_rcnn_r50_fpn_1x_coco_2gpu_base48 
-    #--resume-from=/project/nevatia_174/zhuoming/detection/test/mask_rcnn_r50_fpn_1x_coco_2gpu_base48/latest.pth
+    configs/mask_rcnn/mask_rcnn_r50_fpn_2x_coco_2gpu_base48.py --launcher pytorch \
+    --work-dir=/project/nevatia_174/zhuoming/detection/test/mask_rcnn_r50_fpn_2x_coco_2gpu_base48 \
+    --resume-from=/project/nevatia_174/zhuoming/detection/test/mask_rcnn_r50_fpn_1x_coco_2gpu_base48/epoch_8.pth
