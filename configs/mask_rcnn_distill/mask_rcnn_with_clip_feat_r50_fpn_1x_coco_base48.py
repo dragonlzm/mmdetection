14543bb16be381cd748364e6d9672808ac9bf8b5
dragonlzm <dragonlzm123@gmail.com>
2022-04-05 20:19:46 -0700

    update for scripy

---
+++ configs/mask_rcnn_distill/mask_rcnn_with_clip_feat_r50_fpn_1x_coco_base48.py
@@ -0,0 +1,25 @@
+_base_ = './mask_rcnn_with_clip_feat_r50_fpn_1x_coco.py'
+
+classes = ('person', 'bicycle', 'car', 'motorcycle', 'train', 
+            'truck', 'boat', 'bench', 'bird', 'horse', 'sheep', 
+            'bear', 'zebra', 'giraffe', 'backpack', 'handbag', 
+            'suitcase', 'frisbee', 'skis', 'kite', 'surfboard', 
+            'bottle', 'fork', 'spoon', 'bowl', 'banana', 'apple', 
+            'sandwich', 'orange', 'broccoli', 'carrot', 'pizza', 
+            'donut', 'chair', 'bed', 'toilet', 'tv', 'laptop', 
+            'mouse', 'remote', 'microwave', 'oven', 'toaster', 
+            'refrigerator', 'book', 'clock', 'vase', 'toothbrush')
+
+data = dict(
+    train=dict(classes=classes),
+    val=dict(classes=classes),
+    test=dict(classes=classes))
+
+# model settings
+model = dict(
+    roi_head=dict(
+        bbox_head=dict(num_classes=48,
+                       fg_vec_cfg=dict(fixed_param=True, 
+                                       #load_path='/data2/lwll/zhuoming/detection/embeddings/base_finetuned_48cates.pt',
+                                       load_path='/project/nevatia_174/zhuoming/detection/embeddings/base_finetuned_48cates.pt')),
+        mask_head=dict(num_classes=48)))
---
+++ tools/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_base48.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --partition=gpu 
+#SBATCH --gres=gpu:v100:2
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=30GB
+#SBATCH --time=48:00:00
+#SBATCH --account=nevatia_174
+
+
+module purge
+module load gcc/8.3.0
+#module load cuda/10.1.243
+#./program
+
+cd /project/nevatia_174/zhuoming/code/new_rpn/mmdetection
+#rm -rf ./data
+#ln -sf /project/nevatia_174/zhuoming/detection ./data
+
+# for 1x training
+PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=2 \
+    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+    configs/mask_rcnn_distill/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_base48.py --launcher pytorch \
+    --work-dir=/project/nevatia_174/zhuoming/detection/exp_res/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_base48
+    #--resume-from=/project/nevatia_174/zhuoming/detection/exp_res/mask_rcnn_distill_r50_fpn_1x_coco_2gpu_base48/latest.pth
