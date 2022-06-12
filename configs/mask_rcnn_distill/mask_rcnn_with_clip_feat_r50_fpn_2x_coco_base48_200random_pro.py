fc22a81388768273b877bd938f172aa1774bf6c2
dragonlzm <dragonlzm123@gmail.com>
2022-06-12 09:14:29 -0700

    update for config and shell script

---
+++ configs/mask_rcnn_distill/mask_rcnn_with_clip_feat_r50_fpn_2x_coco_base48_200random_pro.py
@@ -0,0 +1,5 @@
+_base_ = './mask_rcnn_with_clip_feat_r50_fpn_1x_coco_base48_200random_pro.py'
+
+# learning policy
+lr_config = dict(step=[16, 22])
+runner = dict(max_epochs=24)
--- tools/mask_rcnn_with_clip_feat_r50_fpn_1x_coco_base48.sh
+++ tools/mask_rcnn_with_clip_feat_r50_fpn_1x_coco_base48.sh
@@ -18,39 +18,23 @@ cd /project/nevatia_174/zhuoming/code/new_rpn/mmdetection
 #rm -rf ./data
 #ln -sf /project/nevatia_174/zhuoming/detection ./data
 
-# for 1x training
-#PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
-#python -m torch.distributed.launch --nproc_per_node=2 \
-#    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
-#    configs/mask_rcnn_distill/mask_rcnn_with_clip_feat_r50_fpn_1x_coco_base48.py --launcher pytorch \
-#    --work-dir=/project/nevatia_174/zhuoming/detection/exp_res/mask_rcnn_with_clip_feat_r50_fpn_1x_coco_base48
-    #--resume-from=/project/nevatia_174/zhuoming/detection/exp_res/mask_rcnn_distill_r50_fpn_1x_coco_2gpu_base48/latest.pth
-
-# for 1x training
-# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
-# python -m torch.distributed.launch --nproc_per_node=2 \
-#     /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
-#     configs/mask_rcnn_distill/mask_rcnn_with_clip_feat_r50_fpn_1x_coco_base48.py --launcher pytorch \
-#     --work-dir=/project/nevatia_174/zhuoming/detection/reimplement_distillation/mask_rcnn_with_clip_feat_r50_fpn_1x_coco_base48
-#     #--resume-from=/project/nevatia_174/zhuoming/detection/reimplement_distillation/mask_rcnn_with_clip_feat_r50_fpn_1x_coco_base48/latest.pth
-
-# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
-# python -m torch.distributed.launch --nproc_per_node=2 \
-#     /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
-#     configs/mask_rcnn_distill/mask_rcnn_with_clip_feat_r50_fpn_1x_coco_base48.py --launcher pytorch \
-#     --work-dir=/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_clip_feat_r50_fpn_1x_coco_base48 \
-#     --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=1 optimizer_config.grad_clip.max_norm=10
-#     #--resume-from=/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_clip_feat_r50_fpn_1x_coco_base48/latest.pth
-
-
-# for 200 clip propsal raw feat
+# for 100 random proposal raw feat
 PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=2 \
     /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
-    configs/mask_rcnn_distill/mask_rcnn_with_clip_feat_r50_fpn_1x_coco_base48_200clip_pro.py --launcher pytorch \
-    --work-dir=/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_clip_feat_r50_fpn_1x_coco_base48_200clip_pro \
+    configs/mask_rcnn_distill/mask_rcnn_with_clip_feat_r50_fpn_1x_coco_base48.py --launcher pytorch \
+    --work-dir=/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_clip_feat_r50_fpn_1x_coco_base48_old_embed_repro \
     --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=1 optimizer_config.grad_clip.max_norm=10
-    #--resume-from=/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_clip_feat_r50_fpn_1x_coco_base48_200clip_pro/latest.pth
+    #--resume-from=/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_clip_feat_r50_fpn_1x_coco_base48/latest.pth
+
+# for 200 clip propsal raw feat
+# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+# python -m torch.distributed.launch --nproc_per_node=2 \
+#     /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+#     configs/mask_rcnn_distill/mask_rcnn_with_clip_feat_r50_fpn_1x_coco_base48_200clip_pro.py --launcher pytorch \
+#     --work-dir=/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_clip_feat_r50_fpn_1x_coco_base48_200clip_pro \
+#     --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=1 optimizer_config.grad_clip.max_norm=10
+#     #--resume-from=/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_clip_feat_r50_fpn_1x_coco_base48_200clip_pro/latest.pth
 
 # for 200 random proposal raw feat
 # PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
@@ -60,3 +44,32 @@ python -m torch.distributed.launch --nproc_per_node=2 \
 #     --work-dir=/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_clip_feat_r50_fpn_1x_coco_base48_200random_pro \
 #     --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=1 optimizer_config.grad_clip.max_norm=10
 #     #--resume-from=/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_clip_feat_r50_fpn_1x_coco_base48_200random_pro/latest.pth
+
+# for 200 random proposal raw feat 2x
+# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+# python -m torch.distributed.launch --nproc_per_node=2 \
+#     /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+#     configs/mask_rcnn_distill/mask_rcnn_with_clip_feat_r50_fpn_2x_coco_base48_200random_pro.py --launcher pytorch \
+#     --work-dir=/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_clip_feat_r50_fpn_2x_coco_base48_200random_pro \
+#     --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=1 optimizer_config.grad_clip.max_norm=10 \
+#     --resume-from=/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_clip_feat_r50_fpn_1x_coco_base48_200random_pro/epoch_8.pth
+
+# for 200 random proposal raw feat 3x
+# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+# python -m torch.distributed.launch --nproc_per_node=2 \
+#     /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+#     configs/mask_rcnn_distill/mask_rcnn_with_clip_feat_r50_fpn_3x_coco_base48_200random_pro.py --launcher pytorch \
+#     --work-dir=/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_clip_feat_r50_fpn_3x_coco_base48_200random_pro \
+#     --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=1 optimizer_config.grad_clip.max_norm=10 \
+#     --resume-from=/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_clip_feat_r50_fpn_2x_coco_base48_200random_pro/epoch_16.pth
+
+# for 200 random proposal raw feat 4x
+# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+# python -m torch.distributed.launch --nproc_per_node=2 \
+#     /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+#     configs/mask_rcnn_distill/mask_rcnn_with_clip_feat_r50_fpn_4x_coco_base48_200random_pro.py --launcher pytorch \
+#     --work-dir=/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_clip_feat_r50_fpn_4x_coco_base48_200random_pro \
+#     --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=1 optimizer_config.grad_clip.max_norm=10 \
+#     --resume-from=/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_clip_feat_r50_fpn_3x_coco_base48_200random_pro/epoch_24.pth
+
+
