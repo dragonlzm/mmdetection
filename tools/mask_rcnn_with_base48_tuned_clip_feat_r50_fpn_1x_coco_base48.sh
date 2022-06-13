2f6f94100bed6a7a608191cad6f3de949bf290a4
dragonlzm <dragonlzm123@gmail.com>
2022-06-13 12:45:04 -0700

    updates

--- tools/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_base48.sh
+++ tools/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_base48.sh
@@ -117,4 +117,4 @@ python -m torch.distributed.launch --nproc_per_node=2 \
 #     configs/mask_rcnn_distill/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_base48_200clip_pro.py --launcher pytorch \
 #     --work-dir=/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_base48_200clip_pro_distill_w_128 \
 #     --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=0.25 optimizer_config.grad_clip.max_norm=10
-#     #--resume-from=/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_base48_200clip_pro_distill_w_256/latest.pth
+#     #--resume-from=/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_base48_200clip_pro_distill_w_128/latest.pth
