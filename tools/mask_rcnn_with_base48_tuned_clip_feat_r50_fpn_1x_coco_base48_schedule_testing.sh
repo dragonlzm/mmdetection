0ff7c55c96bd366db68815f81b2756e6b02c790d
dragonlzm <dragonlzm123@gmail.com>
2022-06-17 15:49:34 -0700

    update for scheduling

---
+++ tools/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_base48_schedule_testing.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+#SBATCH --partition=gpu 
+#SBATCH --gres=gpu:v100:2
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=30GB
+#SBATCH --time=48:00:00
+#SBATCH --account=nevatia_174
+
+
+module purge
+module load gcc/8.3.0
+#module load cuda/10.1.243
+#./program
+
+cd /project/nevatia_174/zhuoming/code/new_rpn/mmdetection
+#rm -rf ./data
+#ln -sf /project/nevatia_174/zhuoming/detection ./data
+
+# mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_2x822_coco_base48_200clip_pro
+PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=2 \
+    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+    configs/mask_rcnn_distill/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_2x822_coco_base48_200clip_pro.py --launcher pytorch \
+    --work-dir=/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_2x822_coco_base48_200clip_pro \
+    --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=1 optimizer_config.grad_clip.max_norm=10 \
+    --resume-from=/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_base48_gn_10_200clipproposal/epoch_11.pth
+
+# mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_2x820_coco_base48_200clip_pro
+# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+# python -m torch.distributed.launch --nproc_per_node=2 \
+#     /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+#     configs/mask_rcnn_distill/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_2x820_coco_base48_200clip_pro.py --launcher pytorch \
+#     --work-dir=/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_2x820_coco_base48_200clip_pro \
+#     --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=1 optimizer_config.grad_clip.max_norm=10 \
+#     --resume-from=/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_base48_gn_10_200clipproposal/epoch_11.pth
+
+# mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_2x818_coco_base48_200clip_pro
+# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+# python -m torch.distributed.launch --nproc_per_node=2 \
+#     /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+#     configs/mask_rcnn_distill/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_2x818_coco_base48_200clip_pro.py --launcher pytorch \
+#     --work-dir=/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_2x818_coco_base48_200clip_pro \
+#     --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=1 optimizer_config.grad_clip.max_norm=10 \
+#     --resume-from=/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_base48_gn_10_200clipproposal/epoch_11.pth
+
+# mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_2x816_coco_base48_200clip_pro
+# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+# python -m torch.distributed.launch --nproc_per_node=2 \
+#     /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+#     configs/mask_rcnn_distill/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_2x816_coco_base48_200clip_pro.py --launcher pytorch \
+#     --work-dir=/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_2x816_coco_base48_200clip_pro \
+#     --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=1 optimizer_config.grad_clip.max_norm=10 \
+#     --resume-from=/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_base48_gn_10_200clipproposal/epoch_11.pth
