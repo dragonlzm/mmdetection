8f939dd4c85ed0f98cb5abd33938fac263025aa5
dragonlzm <dragonlzm123@gmail.com>
2022-06-17 13:52:29 -0700

    update for script

--- tools/fcos_r50_caffe_fpn_gn_head_1x_coco.sh
+++ tools/fcos_r50_caffe_fpn_gn_head_1x_coco.sh
@@ -18,6 +18,7 @@ cd /project/nevatia_174/zhuoming/code/new_rpn/mmdetection
 #rm -rf ./data
 #ln -sf /project/nevatia_174/zhuoming/detection ./data
 
+# 80 cates
 PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=2 \
    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
@@ -25,3 +26,12 @@ python -m torch.distributed.launch --nproc_per_node=2 \
    --work-dir=/project/nevatia_174/zhuoming/detection/fcos/fcos_r50_caffe_fpn_gn-head_1x_coco \
    --cfg-options optimizer.lr=0.0025
    #--resume-from=/project/nevatia_174/zhuoming/detection/fcos/fcos_r50_caffe_fpn_gn-head_1x_coco/latest.pth
+
+# 48 cates
+PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=2 \
+   /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+   configs/fcos/fcos_r50_caffe_fpn_gn-head_1x_coco_base48.py --launcher pytorch \
+   --work-dir=/project/nevatia_174/zhuoming/detection/fcos/fcos_r50_caffe_fpn_gn-head_1x_coco_base48 \
+   --cfg-options optimizer.lr=0.0025
+   #--resume-from=/project/nevatia_174/zhuoming/detection/fcos/fcos_r50_caffe_fpn_gn-head_1x_coco_base48/latest.pth
