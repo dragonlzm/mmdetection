50cd7e392c53bf49826de67cee2b564ccae60cf9
dragonlzm <dragonlzm123@gmail.com>
2022-06-22 09:32:19 -0700

    update for config and scripts

---
+++ configs/fcos_distill/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_base48_200_clip_pro.py
@@ -0,0 +1,21 @@
+_base_ = './fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_base48.py'
+# using random proposal with clip feat
+img_norm_cfg = dict(
+    mean=[102.9801, 115.9465, 122.7717], std=[1.0, 1.0, 1.0], to_rgb=False)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.0),
+    dict(type='LoadCLIPFeat', file_path_prefix='data/coco/feat/base48_finetuned_500',
+         num_of_rand_bbox=200, select_fixed_subset=200),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_feats',
+                               'rand_bboxes', 'rand_feats']),
+]
+
+data = dict(
+    train=dict(
+        pipeline=train_pipeline))
--- tools/fcos_r50_caffe_fpn_gn-head_with_clip_feat.sh
+++ tools/fcos_r50_caffe_fpn_gn-head_with_clip_feat.sh
@@ -18,14 +18,14 @@ cd /project/nevatia_174/zhuoming/code/new_rpn/mmdetection
 #rm -rf ./data
 #ln -sf /project/nevatia_174/zhuoming/detection ./data
 
-# 48 cates without centerness
-PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=2 \
-   /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
-   configs/fcos_distill/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_base48.py --launcher pytorch \
-   --work-dir=/project/nevatia_174/zhuoming/detection/fcos/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_base48_w01 \
-   --cfg-options model.train_cfg.distill_loss_factor=0.1
-   #--resume-from=/project/nevatia_174/zhuoming/detection/fcos/fcos_r50_caffe_fpn_gn-head_1x_coco_wo_centerness_base48/latest.pth
+# 48 cates 100 clip proposal
+# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+# python -m torch.distributed.launch --nproc_per_node=2 \
+#    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+#    configs/fcos_distill/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_base48.py --launcher pytorch \
+#    --work-dir=/project/nevatia_174/zhuoming/detection/fcos/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_base48_w01 \
+#    --cfg-options model.train_cfg.distill_loss_factor=0.1
+#    #--resume-from=/project/nevatia_174/zhuoming/detection/fcos/fcos_r50_caffe_fpn_gn-head_1x_coco_wo_centerness_base48/latest.pth
 
 # PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
 # python -m torch.distributed.launch --nproc_per_node=2 \
@@ -43,10 +43,46 @@ python -m torch.distributed.launch --nproc_per_node=2 \
 #    --cfg-options model.train_cfg.distill_loss_factor=0.5
 #    #--resume-from=/project/nevatia_174/zhuoming/detection/fcos/fcos_r50_caffe_fpn_gn-head_1x_coco_wo_centerness_base48/latest.pth
 
+# 48 cates 200 clip proposal
+PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=2 \
+   /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+   configs/fcos_distill/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_base48_200_clip_pro.py --launcher pytorch \
+   --work-dir=/project/nevatia_174/zhuoming/detection/fcos/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_base48_200_clip_pro_w01 \
+   --cfg-options model.train_cfg.distill_loss_factor=0.1
+   #--resume-from=/project/nevatia_174/zhuoming/detection/fcos/fcos_r50_caffe_fpn_gn-head_1x_coco_wo_centerness_base48/latest.pth
+
+# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+# python -m torch.distributed.launch --nproc_per_node=2 \
+#    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+#    configs/fcos_distill/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_base48_200_clip_pro.py --launcher pytorch \
+#    --work-dir=/project/nevatia_174/zhuoming/detection/fcos/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_base48_200_clip_pro_w002 \
+#    --cfg-options model.train_cfg.distill_loss_factor=0.02
+#    #--resume-from=/project/nevatia_174/zhuoming/detection/fcos/fcos_r50_caffe_fpn_gn-head_1x_coco_wo_centerness_base48/latest.pth
+
+# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+# python -m torch.distributed.launch --nproc_per_node=2 \
+#    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+#    configs/fcos_distill/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_base48_200_clip_pro.py --launcher pytorch \
+#    --work-dir=/project/nevatia_174/zhuoming/detection/fcos/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_base48_200_clip_pro_w05 \
+#    --cfg-options model.train_cfg.distill_loss_factor=0.5
+#    #--resume-from=/project/nevatia_174/zhuoming/detection/fcos/fcos_r50_caffe_fpn_gn-head_1x_coco_wo_centerness_base48/latest.pth
 
 # test the model 
 bash tools/dist_test.sh configs/fcos_distill/fcos_r50_caffe_fpn_gn-head_with_clip_feat_1x_coco_base48.py \
-data/fcos/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_base48_w01/epoch_12.pth 2 --eval bbox \
---eval-options jsonfile_prefix=data/fcos/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_base48_w01/base \
+data/fcos/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_base48_200_clip_pro_w01/epoch_12.pth 2 --eval bbox \
+--eval-options jsonfile_prefix=data/fcos/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_base48_200_clip_pro_w01/base \
+--cfg-options data.test.eval_filter_empty_gt=False data.test.ann_file=data/coco/annotations/instances_val2017_65cates.json \
+model.test_cfg.score_thr=0.0 model.test_cfg.max_per_img=300
+
+bash tools/dist_test.sh configs/fcos_distill/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_base48.py \
+/data/zhuoming/detection/fcos/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_base48_200_clip_pro_w01/epoch_12.pth 3 --eval bbox \
+--eval-options jsonfile_prefix=/data/zhuoming/detection/fcos/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_base48_200_clip_pro_w01/base_results \
+--cfg-options data.test.eval_filter_empty_gt=False data.test.ann_file=data/coco/annotations/instances_val2017_65cates.json \
+model.test_cfg.score_thr=0.0 model.test_cfg.max_per_img=300
+
+bash tools/dist_test.sh configs/fcos_distill/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_novel17.py \
+/data/zhuoming/detection/fcos/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_base48_200_clip_pro_w01/epoch_12.pth 3 --eval bbox \
+--eval-options jsonfile_prefix=/data/zhuoming/detection/fcos/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_base48_200_clip_pro_w01/novel_results \
 --cfg-options data.test.eval_filter_empty_gt=False data.test.ann_file=data/coco/annotations/instances_val2017_65cates.json \
 model.test_cfg.score_thr=0.0 model.test_cfg.max_per_img=300
