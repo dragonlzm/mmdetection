2017f9300207cd9cd8ae0385f732250e697227d5
dragonlzm <dragonlzm123@gmail.com>
2022-07-07 11:19:44 -0700

    update for file

--- tools/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_base48.sh
+++ tools/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_base48.sh
@@ -419,7 +419,6 @@ python -m torch.distributed.launch --nproc_per_node=2 \
     --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=1 optimizer_config.grad_clip.max_norm=10 \
     --resume-from=${START_FROM}/epoch_16.pth
 
-
 # rn50 initialization
 # WORK_DIR="/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_base48_200clip_pro_with_clip_pretrain"
 # PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
@@ -429,17 +428,6 @@ python -m torch.distributed.launch --nproc_per_node=2 \
 #     --work-dir=${WORK_DIR} \
 #     --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=1 optimizer_config.grad_clip.max_norm=10 \
 #     #--resume-from=${WORK_DIR}/latest.pth
-
-# c4 model
-# WORK_DIR="/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_base48_tuned_clip_feat_r50_caffe_c4_1x_coco_base48_200clip_pro_with_flip"
-# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
-# python -m torch.distributed.launch --nproc_per_node=2 \
-#     /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
-#     configs/mask_rcnn_distill/mask_rcnn_with_base48_tuned_clip_feat_r50_caffe_c4_1x_coco_base48_200clip_pro_with_flip.py --launcher pytorch \
-#     --work-dir=${WORK_DIR} \
-#     --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=1 optimizer_config.grad_clip.max_norm=10 \
-#     #--resume-from=${WORK_DIR}/latest.pth
-
 
 # test the model
 #CHECKPOINT_NAME="epoch_12.pth"
