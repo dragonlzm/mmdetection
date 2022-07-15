2f8d9478796d777ca6eb8d0360bf99f0b15112f6
dragonlzm <dragonlzm123@gmail.com>
2022-07-15 08:56:17 -0700

    update for shell script

--- tools/attention_rpn_base_training.sh
+++ tools/attention_rpn_base_training.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --partition=gpu 
-#SBATCH --gres=gpu:v100:2
+#SBATCH --gres=gpu:p100:2
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=60GB
@@ -19,19 +19,19 @@ cd /project/nevatia_174/zhuoming/code/new_rpn/mmdetection
 #ln -sf /project/nevatia_174/zhuoming/detection ./data
 
 # 2*2 batch size
-WORK_DIR="/project/nevatia_174/zhuoming/detection/meta_learning/attention-rpn_r50_c4_4xb2_coco_official-base-training_2gpu"
+WORK_DIR="/project/nevatia_174/zhuoming/detection/meta_learning/attention-rpn_r50_c4_2xb2_coco_official-base-training"
 PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=2 \
     /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/few_shot_train.py \
-	configs/attention-rpn/attention-rpn_r50_c4_4xb2_coco_official-base-training_2gpu.py --launcher pytorch \
+	configs/attention-rpn/attention-rpn_r50_c4_2xb2_coco_official-base-training.py --launcher pytorch \
 	--work-dir=${WORK_DIR} \
 	#--resume-from=${WORK_DIR}/latest.pth
 
 # 4*2 batch size
-WORK_DIR="/project/nevatia_174/zhuoming/detection/meta_learning/attention-rpn_r50_c4_4xb2_coco_official-base-training_2gpu_bs8"
+WORK_DIR="/project/nevatia_174/zhuoming/detection/meta_learning/attention-rpn_r50_c4_2xb4_coco_official-base-training"
 PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
 python -m torch.distributed.launch --nproc_per_node=2 \
     /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/few_shot_train.py \
-	configs/attention-rpn/attention-rpn_r50_c4_4xb2_coco_official-base-training_2gpu_bs8.py --launcher pytorch \
+	configs/attention-rpn/attention-rpn_r50_c4_2xb4_coco_official-base-training.py --launcher pytorch \
 	--work-dir=${WORK_DIR} \
 	#--resume-from=${WORK_DIR}/latest.pth
