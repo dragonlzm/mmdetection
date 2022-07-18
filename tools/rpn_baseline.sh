82e51f4ff484acd673a6e69578e7548f47c3f8e3
dragonlzm <dragonlzm123@gmail.com>
2022-07-18 00:10:14 -0700

    update for shell script

--- tools/rpn_baseline.sh
+++ tools/rpn_baseline.sh
@@ -20,17 +20,17 @@ cd /project/nevatia_174/zhuoming/code/new_rpn/mmdetection
 
 
 # rpn base48
-TRAIN_CONFIG="configs/rpn/rpn_r50_caffe_c4_1x_coco_base48.py"
-TEST_CONFIG="configs/rpn/rpn_r50_caffe_c4_1x_coco_novel17.py"
-WORK_DIR="/project/nevatia_174/zhuoming/detection/baseline/rpn_r50_caffe_c4_1x_coco_base48"
-PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=2 \
-    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
-    ${TRAIN_CONFIG} --launcher pytorch \
-    --work-dir=${WORK_DIR} \
-    #--resume-from=${WORK_DIR}/latest.pth
+# TRAIN_CONFIG="configs/rpn/rpn_r50_caffe_c4_1x_coco_base48.py"
+# TEST_CONFIG="configs/rpn/rpn_r50_caffe_c4_1x_coco_novel17.py"
+# WORK_DIR="/project/nevatia_174/zhuoming/detection/baseline/rpn_r50_caffe_c4_1x_coco_base48"
+# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+# python -m torch.distributed.launch --nproc_per_node=2 \
+#     /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+#     ${TRAIN_CONFIG} --launcher pytorch \
+#     --work-dir=${WORK_DIR} \
+#     #--resume-from=${WORK_DIR}/latest.pth
 
-# rpn base48
+# rpn base60
 # TRAIN_CONFIG="configs/rpn/rpn_r50_caffe_c4_1x_coco_base60.py"
 # TEST_CONFIG="configs/rpn/rpn_r50_caffe_c4_1x_coco_novel20.py"
 # WORK_DIR="/project/nevatia_174/zhuoming/detection/baseline/rpn_r50_caffe_c4_1x_coco_base60"
@@ -41,10 +41,34 @@ python -m torch.distributed.launch --nproc_per_node=2 \
 #     --work-dir=${WORK_DIR} \
 #     #--resume-from=${WORK_DIR}/latest.pth
 
+
+# rpn base48 fpn
+ALL_CONFIG="configs/rpn/rpn_r50_fpn_1x_coco.py"
+TRAIN_CONFIG="configs/rpn/rpn_r50_fpn_1x_coco_base48.py"
+TEST_CONFIG="configs/rpn/rpn_r50_fpn_1x_coco_novel17.py"
+WORK_DIR="/project/nevatia_174/zhuoming/detection/baseline/rpn_r50_fpn_1x_coco_base48"
+PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=2 \
+    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+    ${TRAIN_CONFIG} --launcher pytorch \
+    --work-dir=${WORK_DIR} \
+    #--resume-from=${WORK_DIR}/latest.pth
+
+
 # test the model
 CHECKPOINT_NAME="epoch_12.pth"
 #CHECKPOINT_NAME="epoch_24.pth"
 
-bash tools/dist_test.sh ${TEST_CONFIG}
-${WORK_DIR}/${CHECKPOINT_NAME} 2 --eval=proposal
---eval-options jsonfile_prefix=${WORK_DIR}/test_novel
+bash tools/dist_test.sh ${ALL_CONFIG} \
+${WORK_DIR}/${CHECKPOINT_NAME} 2 \
+--eval=proposal_fast --options jsonfile_prefix=${WORK_DIR}/test_all
+
+bash tools/dist_test.sh ${TRAIN_CONFIG} \
+${WORK_DIR}/${CHECKPOINT_NAME} 2 \
+--eval=proposal_fast --options jsonfile_prefix=${WORK_DIR}/test_48
+
+bash tools/dist_test.sh ${TEST_CONFIG} \
+${WORK_DIR}/${CHECKPOINT_NAME} 2 \
+--eval=proposal_fast --options jsonfile_prefix=${WORK_DIR}/test_17
+
+
