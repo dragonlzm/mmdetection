b80955e5e2e4a6393fe562411d267d79342fae6e
dragonlzm <dragonlzm123@gmail.com>
2022-07-17 23:42:23 -0700

    update for config

--- configs/rpn/rpn_r50_fpn_1x_coco_testing.py
+++
@@ -1,6 +0,0 @@
-_base_ = './rpn_r50_fpn_1x_coco.py'
-
-
-model = dict(
-    backbone=dict(
-        init_cfg=dict(type='Pretrained', checkpoint='/data2/lwll/zhuoming/detection/test/rpn/latest.pth')))
