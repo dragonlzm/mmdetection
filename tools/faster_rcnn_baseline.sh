8d28561348306fb9517cf9a61ee5b722c2ae84b3
dragonlzm <dragonlzm123@gmail.com>
2022-07-19 16:56:02 -0700

    update for shell script

--- tools/faster_rcnn_baseline.sh
+++ tools/faster_rcnn_baseline.sh
@@ -20,19 +20,21 @@ cd /project/nevatia_174/zhuoming/code/new_rpn/mmdetection
 
 
 # faster rcnn base48
-# TRAIN_CONFIG="configs/faster_rcnn/faster_rcnn_r50_caffe_c4_1x_coco_base48.py"
-# TEST_CONFIG="configs/rpn/rpn_r50_caffe_c4_1x_coco_novel17.py"
-# WORK_DIR="/project/nevatia_174/zhuoming/detection/baseline/faster_rcnn_r50_caffe_c4_1x_coco_base48"
-# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
-# python -m torch.distributed.launch --nproc_per_node=2 \
-#     /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
-#     ${TRAIN_CONFIG} --launcher pytorch \
-#     --work-dir=${WORK_DIR} \
-#     #--resume-from=${WORK_DIR}/latest.pth
+TRAIN_CONFIG="configs/faster_rcnn/faster_rcnn_r50_caffe_c4_1x_coco_base48.py"
+BASE_TEST_CONFIG="configs/rpn/rpn_r50_caffe_c4_1x_coco_base48.py"
+NOVEL_TEST_CONFIG="configs/rpn/rpn_r50_caffe_c4_1x_coco_novel17.py"
+WORK_DIR="/project/nevatia_174/zhuoming/detection/baseline/faster_rcnn_r50_caffe_c4_1x_coco_base48"
+PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=2 \
+    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+    ${TRAIN_CONFIG} --launcher pytorch \
+    --work-dir=${WORK_DIR} \
+    #--resume-from=${WORK_DIR}/latest.pth
 
 # faster rcnn base60
 # TRAIN_CONFIG="configs/faster_rcnn/faster_rcnn_r50_caffe_c4_1x_coco_base60.py"
-# TEST_CONFIG="configs/rpn/rpn_r50_caffe_c4_1x_coco_novel20.py"
+# BASE_TEST_CONFIG="configs/rpn/rpn_r50_caffe_c4_1x_coco_base60.py"
+# NOVEL_TEST_CONFIG="configs/rpn/rpn_r50_caffe_c4_1x_coco_novel20.py"
 # WORK_DIR="/project/nevatia_174/zhuoming/detection/baseline/faster_rcnn_r50_caffe_c4_1x_coco_base60"
 # PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
 # python -m torch.distributed.launch --nproc_per_node=2 \
@@ -42,31 +44,42 @@ cd /project/nevatia_174/zhuoming/code/new_rpn/mmdetection
 #     #--resume-from=${WORK_DIR}/latest.pth
 
 # faster rcnn base48
-TRAIN_CONFIG="configs/faster_rcnn/faster_rcnn_r50_caffe_c4_1x_coco_with_pretrain_base48.py"
-TEST_CONFIG="configs/rpn/rpn_r50_caffe_c4_1x_coco_novel17.py"
-WORK_DIR="/project/nevatia_174/zhuoming/detection/baseline/faster_rcnn_r50_caffe_c4_1x_coco_with_pretrain_base48"
-PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=2 \
-    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
-    ${TRAIN_CONFIG} --launcher pytorch \
-    --work-dir=${WORK_DIR} \
-    #--resume-from=${WORK_DIR}/latest.pth
+# TRAIN_CONFIG="configs/faster_rcnn/faster_rcnn_r50_caffe_c4_1x_coco_with_pretrain_base48.py"
+# BASE_TEST_CONFIG="configs/rpn/rpn_r50_caffe_c4_1x_coco_base48.py"
+# NOVEL_TEST_CONFIG="configs/rpn/rpn_r50_caffe_c4_1x_coco_novel17.py"
+# WORK_DIR="/project/nevatia_174/zhuoming/detection/baseline/faster_rcnn_r50_caffe_c4_1x_coco_with_pretrain_base48"
+# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+# python -m torch.distributed.launch --nproc_per_node=2 \
+#     /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+#     ${TRAIN_CONFIG} --launcher pytorch \
+#     --work-dir=${WORK_DIR} \
+#     #--resume-from=${WORK_DIR}/latest.pth
 
 # faster rcnn base60
-TRAIN_CONFIG="configs/faster_rcnn/faster_rcnn_r50_caffe_c4_1x_coco_with_pretrain_base60.py"
-TEST_CONFIG="configs/rpn/rpn_r50_caffe_c4_1x_coco_novel20.py"
-WORK_DIR="/project/nevatia_174/zhuoming/detection/baseline/faster_rcnn_r50_caffe_c4_1x_coco_with_pretrain_base60"
-PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=2 \
-    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
-    ${TRAIN_CONFIG} --launcher pytorch \
-    --work-dir=${WORK_DIR} \
-    #--resume-from=${WORK_DIR}/latest.pth
+# TRAIN_CONFIG="configs/faster_rcnn/faster_rcnn_r50_caffe_c4_1x_coco_with_pretrain_base60.py"
+# BASE_TEST_CONFIG="configs/rpn/rpn_r50_caffe_c4_1x_coco_base60.py"
+# NOVEL_TEST_CONFIG="configs/rpn/rpn_r50_caffe_c4_1x_coco_novel20.py"
+# WORK_DIR="/project/nevatia_174/zhuoming/detection/baseline/faster_rcnn_r50_caffe_c4_1x_coco_with_pretrain_base60"
+# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+# python -m torch.distributed.launch --nproc_per_node=2 \
+#     /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+#     ${TRAIN_CONFIG} --launcher pytorch \
+#     --work-dir=${WORK_DIR} \
+#     #--resume-from=${WORK_DIR}/latest.pth
 
 # test the model
 CHECKPOINT_NAME="epoch_12.pth"
 #CHECKPOINT_NAME="epoch_24.pth"
+ALL_CONFIG="configs/rpn/rpn_r50_fpn_1x_coco.py"
 
-bash tools/dist_test.sh ${TEST_CONFIG}
-${WORK_DIR}/${CHECKPOINT_NAME} 2 --eval=proposal
---eval-options jsonfile_prefix=${WORK_DIR}/test_novel
+bash tools/dist_test.sh ${ALL_CONFIG} \
+${WORK_DIR}/${CHECKPOINT_NAME} 2 \
+--eval=proposal_fast --options jsonfile_prefix=${WORK_DIR}/test_all
+
+bash tools/dist_test.sh ${BASE_TEST_CONFIG} \
+${WORK_DIR}/${CHECKPOINT_NAME} 2 \
+--eval=proposal_fast --options jsonfile_prefix=${WORK_DIR}/test_base
+
+bash tools/dist_test.sh ${NOVEL_TEST_CONFIG} \
+${WORK_DIR}/${CHECKPOINT_NAME} 2 \
+--eval=proposal_fast --options jsonfile_prefix=${WORK_DIR}/test_novel
