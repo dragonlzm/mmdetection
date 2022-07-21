6bd83eb40945543ae6d95bc924dfd7333eb970fe
dragonlzm <dragonlzm123@gmail.com>
2022-07-20 19:41:51 -0700

    update for script

--- tools/rpn_attention_rpn_testing_and_finetuning.sh
+++ tools/rpn_attention_rpn_testing_and_finetuning.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --partition=gpu 
-#SBATCH --gres=gpu:v100:2
+#SBATCH --gres=gpu:p100:2
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=80GB
@@ -18,6 +18,8 @@ cd /project/nevatia_174/zhuoming/code/new_rpn/mmdetection
 #rm -rf ./data
 #ln -sf /project/nevatia_174/zhuoming/detection ./data
 
+EXTRA_CONFIG=""
+
 # for image fewshot
 # WORK_DIR="/project/nevatia_174/zhuoming/detection/meta_learning/rpn_attention-rpn_r50_c4_2xb2_coco_official-base-training"
 # CONFIG_FILE="configs/attention-rpn/rpn_attention-rpn_r50_c4_coco_official-10shot-fine-tuning.py"
@@ -29,10 +31,10 @@ cd /project/nevatia_174/zhuoming/code/new_rpn/mmdetection
 # BASE_CONFIG_FILE="configs/attention-rpn/rpn_attention-rpn_r50_c4_2xb4_coco_official-base-training.py"
 # CHECKPOINT_NAME="iter_120000.pth"
 
-WORK_DIR="/project/nevatia_174/zhuoming/detection/meta_learning/rpn_attention-rpn_r50_c4_2xb4_coco_official-base48-training"
-CONFIG_FILE="configs/attention-rpn/rpn_attention-rpn_r50_c4_coco_official-10shot-fine-tuning-novel17.py"
-BASE_CONFIG_FILE="configs/attention-rpn/rpn_attention-rpn_r50_c4_2xb4_coco_official-base48-training.py"
-CHECKPOINT_NAME="iter_120000.pth"
+# WORK_DIR="/project/nevatia_174/zhuoming/detection/meta_learning/rpn_attention-rpn_r50_c4_2xb4_coco_official-base48-training"
+# CONFIG_FILE="configs/attention-rpn/rpn_attention-rpn_r50_c4_coco_official-10shot-fine-tuning-novel17.py"
+# BASE_CONFIG_FILE="configs/attention-rpn/rpn_attention-rpn_r50_c4_2xb4_coco_official-base48-training.py"
+# CHECKPOINT_NAME="iter_120000.pth"
 
 
 # for text zeroshot
@@ -46,25 +48,64 @@ CHECKPOINT_NAME="iter_120000.pth"
 # BASE_CONFIG_FILE="configs/attention-rpn/rpn_attention_text_rpn_r50_c4_2xb4_coco_official-base-training.py"
 # CHECKPOINT_NAME="iter_120000.pth"
 
-WORK_DIR="/project/nevatia_174/zhuoming/detection/meta_learning/rpn_attention_text_rpn_r50_c4_2xb4_coco_official-base48-training"
+# WORK_DIR="/project/nevatia_174/zhuoming/detection/meta_learning/rpn_attention_text_rpn_r50_c4_2xb4_coco_official-base48-training"
+# CONFIG_FILE="configs/attention-rpn/rpn_attention_text_rpn_r50_c4_coco_official-10shot-fine-tuning-novel17.py"
+# BASE_CONFIG_FILE="configs/attention-rpn/rpn_attention_text_rpn_r50_c4_2xb4_coco_official-base48-training.py"
+# CHECKPOINT_NAME="iter_120000.pth"
+
+WORK_DIR="/project/nevatia_174/zhuoming/detection/meta_learning/rpn_attention_text_rpn_r50_c4_2xb4_coco_official-base-training_with_norm"
+CONFIG_FILE="configs/attention-rpn/rpn_attention_text_rpn_r50_c4_coco_official-10shot-fine-tuning.py"
+BASE_CONFIG_FILE="configs/attention-rpn/rpn_attention_text_rpn_r50_c4_2xb4_coco_official-base-training.py"
+CHECKPOINT_NAME="iter_120000.pth"
+EXTRA_CONFIG="--cfg-options model.rpn_head.normalize_img_feat=True"
+
+WORK_DIR="/project/nevatia_174/zhuoming/detection/meta_learning/rpn_attention_text_rpn_r50_c4_2xb4_coco_official-base48-training_with_norm"
 CONFIG_FILE="configs/attention-rpn/rpn_attention_text_rpn_r50_c4_coco_official-10shot-fine-tuning-novel17.py"
 BASE_CONFIG_FILE="configs/attention-rpn/rpn_attention_text_rpn_r50_c4_2xb4_coco_official-base48-training.py"
 CHECKPOINT_NAME="iter_120000.pth"
+EXTRA_CONFIG="--cfg-options model.rpn_head.normalize_img_feat=True"
+
+
+WORK_DIR="/project/nevatia_174/zhuoming/detection/meta_learning/rpn_attention_text_rpn_r50_c4_2xb4_coco_official-base-training_map_on_query"
+CONFIG_FILE="configs/attention-rpn/rpn_attention_text_rpn_r50_c4_coco_official-10shot-fine-tuning.py"
+BASE_CONFIG_FILE="configs/attention-rpn/rpn_attention_text_rpn_r50_c4_2xb4_coco_official-base-training.py"
+CHECKPOINT_NAME="iter_120000.pth"
+EXTRA_CONFIG="--cfg-options model.rpn_head.normalize_img_feat=True --cfg-options model.rpn_head.linear_mapping=on_query"
+
+WORK_DIR="/project/nevatia_174/zhuoming/detection/meta_learning/rpn_attention_text_rpn_r50_c4_2xb4_coco_official-base48-training_map_on_query"
+CONFIG_FILE="configs/attention-rpn/rpn_attention_text_rpn_r50_c4_coco_official-10shot-fine-tuning-novel17.py"
+BASE_CONFIG_FILE="configs/attention-rpn/rpn_attention_text_rpn_r50_c4_2xb4_coco_official-base48-training.py"
+CHECKPOINT_NAME="iter_120000.pth"
+EXTRA_CONFIG="--cfg-options model.rpn_head.normalize_img_feat=True --cfg-options model.rpn_head.linear_mapping=on_query"
+
+
+WORK_DIR="/project/nevatia_174/zhuoming/detection/meta_learning/rpn_attention_text_rpn_r50_c4_2xb4_coco_official-base-training_map_on_both"
+CONFIG_FILE="configs/attention-rpn/rpn_attention_text_rpn_r50_c4_coco_official-10shot-fine-tuning.py"
+BASE_CONFIG_FILE="configs/attention-rpn/rpn_attention_text_rpn_r50_c4_2xb4_coco_official-base-training.py"
+CHECKPOINT_NAME="iter_120000.pth"
+EXTRA_CONFIG="--cfg-options model.rpn_head.normalize_img_feat=True --cfg-options model.rpn_head.linear_mapping=on_both"
+
+WORK_DIR="/project/nevatia_174/zhuoming/detection/meta_learning/rpn_attention_text_rpn_r50_c4_2xb4_coco_official-base48-training_map_on_both"
+CONFIG_FILE="configs/attention-rpn/rpn_attention_text_rpn_r50_c4_coco_official-10shot-fine-tuning-novel17.py"
+BASE_CONFIG_FILE="configs/attention-rpn/rpn_attention_text_rpn_r50_c4_2xb4_coco_official-base48-training.py"
+CHECKPOINT_NAME="iter_120000.pth"
+EXTRA_CONFIG="--cfg-options model.rpn_head.normalize_img_feat=True --cfg-options model.rpn_head.linear_mapping=on_both"
 
 
 # test the model on novel before the finetuning
 bash tools/dist_fewshot_test.sh ${CONFIG_FILE} \
 ${WORK_DIR}/${CHECKPOINT_NAME} 2 --eval proposal_fast \
---eval-options jsonfile_prefix=${WORK_DIR}/test_novel
-
+--eval-options jsonfile_prefix=${WORK_DIR}/test_novel \
+${EXTRA_CONFIG}
 
 # finetune the model and get the performance after finetuning
 bash tools/new_dist_fewshot_train.sh ${CONFIG_FILE} 2 \
 ${WORK_DIR}/finetuning /project/nevatia_174/zhuoming/detection --cfg-options \
-load_from=${WORK_DIR}/${CHECKPOINT_NAME}
-
+load_from=${WORK_DIR}/${CHECKPOINT_NAME} \
+${EXTRA_CONFIG}
 
 # test the model on base after the finetuning
 bash tools/dist_fewshot_test.sh ${BASE_CONFIG_FILE} \
 ${WORK_DIR}/finetuning/latest.pth 2 --eval proposal_fast \
---eval-options jsonfile_prefix=${WORK_DIR}/finetuning/rpn_base_finetuned
+--eval-options jsonfile_prefix=${WORK_DIR}/finetuning/rpn_base_finetuned \
+${EXTRA_CONFIG}
