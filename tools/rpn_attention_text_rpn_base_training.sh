fb40db082f8e601495805f74a88783976abefed1
dragonlzm <dragonlzm123@gmail.com>
2022-07-20 23:32:01 -0700

    update for shell script

--- tools/rpn_attention_text_rpn_base_training.sh
+++ tools/rpn_attention_text_rpn_base_training.sh
@@ -107,17 +107,17 @@ cd /project/nevatia_174/zhuoming/code/new_rpn/mmdetection
 
 
 # 4*2 batch size (w/o any norm, map on query)
-WORK_DIR="/project/nevatia_174/zhuoming/detection/meta_learning/rpn_attention_text_rpn_r50_c4_2xb4_coco_official-base-training_map_on_query_wo_norm"
-FINETUNING_CONFIG_FILE="configs/attention-rpn/rpn_attention_text_rpn_r50_c4_coco_official-10shot-fine-tuning.py"
-BASE_CONFIG_FILE="configs/attention-rpn/rpn_attention_text_rpn_r50_c4_2xb4_coco_official-base-training.py"
-EXTRA_CONFIG="--cfg-options model.rpn_head.linear_mapping=on_query"
-PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=2 \
-    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/few_shot_train.py \
-	${BASE_CONFIG_FILE} --launcher pytorch \
-	--work-dir=${WORK_DIR} \
-	${EXTRA_CONFIG} \
-	#--resume-from=${WORK_DIR}/latest.pth
+# WORK_DIR="/project/nevatia_174/zhuoming/detection/meta_learning/rpn_attention_text_rpn_r50_c4_2xb4_coco_official-base-training_map_on_query_wo_norm"
+# FINETUNING_CONFIG_FILE="configs/attention-rpn/rpn_attention_text_rpn_r50_c4_coco_official-10shot-fine-tuning.py"
+# BASE_CONFIG_FILE="configs/attention-rpn/rpn_attention_text_rpn_r50_c4_2xb4_coco_official-base-training.py"
+# EXTRA_CONFIG="--cfg-options model.rpn_head.linear_mapping=on_query"
+# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+# python -m torch.distributed.launch --nproc_per_node=2 \
+#     /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/few_shot_train.py \
+# 	${BASE_CONFIG_FILE} --launcher pytorch \
+# 	--work-dir=${WORK_DIR} \
+# 	${EXTRA_CONFIG} \
+# 	#--resume-from=${WORK_DIR}/latest.pth
 
 # 4*2 batch size base48 (w/o any norm, map on query)
 # WORK_DIR="/project/nevatia_174/zhuoming/detection/meta_learning/rpn_attention_text_rpn_r50_c4_2xb4_coco_official-base48-training_map_on_query_wo_norm"
@@ -157,6 +157,60 @@ python -m torch.distributed.launch --nproc_per_node=2 \
 # 	--work-dir=${WORK_DIR} \
 # 	${EXTRA_CONFIG} \
 # 	#--resume-from=${WORK_DIR}/latest.pth
+
+
+
+# 4*2 batch size (with support norm, map on query)
+WORK_DIR="/project/nevatia_174/zhuoming/detection/meta_learning/rpn_attention_text_rpn_r50_c4_2xb4_coco_official-base-training_map_on_query_w_sup_norm"
+FINETUNING_CONFIG_FILE="configs/attention-rpn/rpn_attention_text_rpn_r50_c4_coco_official-10shot-fine-tuning.py"
+BASE_CONFIG_FILE="configs/attention-rpn/rpn_attention_text_rpn_r50_c4_2xb4_coco_official-base-training.py"
+EXTRA_CONFIG="--cfg-options model.rpn_head.linear_mapping=on_query model.rpn_head.normalize_text_feat=True"
+PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=2 \
+    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/few_shot_train.py \
+	${BASE_CONFIG_FILE} --launcher pytorch \
+	--work-dir=${WORK_DIR} \
+	${EXTRA_CONFIG} \
+	#--resume-from=${WORK_DIR}/latest.pth
+
+# 4*2 batch size base48 (with support norm, map on query)
+WORK_DIR="/project/nevatia_174/zhuoming/detection/meta_learning/rpn_attention_text_rpn_r50_c4_2xb4_coco_official-base48-training_map_on_query_w_sup_norm"
+FINETUNING_CONFIG_FILE="configs/attention-rpn/rpn_attention_text_rpn_r50_c4_coco_official-10shot-fine-tuning-novel17.py"
+BASE_CONFIG_FILE="configs/attention-rpn/rpn_attention_text_rpn_r50_c4_2xb4_coco_official-base48-training.py"
+EXTRA_CONFIG="--cfg-options model.rpn_head.linear_mapping=on_query model.rpn_head.normalize_text_feat=True"
+PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=2 \
+    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/few_shot_train.py \
+	${BASE_CONFIG_FILE} --launcher pytorch \
+	--work-dir=${WORK_DIR} \
+	${EXTRA_CONFIG} \
+	#--resume-from=${WORK_DIR}/latest.pth
+
+# 4*2 batch size (with support norm, map on both)
+WORK_DIR="/project/nevatia_174/zhuoming/detection/meta_learning/rpn_attention_text_rpn_r50_c4_2xb4_coco_official-base-training_map_on_both_w_sup_norm"
+FINETUNING_CONFIG_FILE="configs/attention-rpn/rpn_attention_text_rpn_r50_c4_coco_official-10shot-fine-tuning.py"
+BASE_CONFIG_FILE="configs/attention-rpn/rpn_attention_text_rpn_r50_c4_2xb4_coco_official-base-training.py"
+EXTRA_CONFIG="--cfg-options model.rpn_head.linear_mapping=on_both model.rpn_head.normalize_text_feat=True"
+PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=2 \
+    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/few_shot_train.py \
+	${BASE_CONFIG_FILE} --launcher pytorch \
+	--work-dir=${WORK_DIR} \
+	${EXTRA_CONFIG} \
+	#--resume-from=${WORK_DIR}/latest.pth
+
+# 4*2 batch size base48 (with support norm, map on both)
+WORK_DIR="/project/nevatia_174/zhuoming/detection/meta_learning/rpn_attention_text_rpn_r50_c4_2xb4_coco_official-base48-training_map_on_both_w_sup_norm"
+FINETUNING_CONFIG_FILE="configs/attention-rpn/rpn_attention_text_rpn_r50_c4_coco_official-10shot-fine-tuning-novel17.py"
+BASE_CONFIG_FILE="configs/attention-rpn/rpn_attention_text_rpn_r50_c4_2xb4_coco_official-base48-training.py"
+EXTRA_CONFIG="--cfg-options model.rpn_head.linear_mapping=on_both model.rpn_head.normalize_text_feat=True"
+PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=2 \
+    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/few_shot_train.py \
+	${BASE_CONFIG_FILE} --launcher pytorch \
+	--work-dir=${WORK_DIR} \
+	${EXTRA_CONFIG} \
+	#--resume-from=${WORK_DIR}/latest.pth
 
 
 # testing and finetuning
