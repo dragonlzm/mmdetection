b89e4fcd0438ede0b0bee98828d51d7449a35a21
dragonlzm <dragonlzm123@gmail.com>
2022-07-22 16:52:29 -0700

    update for mask rcnn iteration base config

---
+++ configs/mask_rcnn/mask_rcnn_r50_fpn_1x_coco_detectron_2gpu.py
@@ -0,0 +1,20 @@
+_base_ = [
+    '../_base_/models/mask_rcnn_r50_fpn.py',
+    '../_base_/datasets/coco_instance.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
+
+optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None)
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=0.001,
+    step=[60000, 80000])
+runner = dict(type='IterBasedRunner', max_iters=90000)
+
+data = dict(
+    samples_per_gpu=8,
+    workers_per_gpu=4)
