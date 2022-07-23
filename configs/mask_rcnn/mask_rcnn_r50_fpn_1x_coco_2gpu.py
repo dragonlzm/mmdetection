c65f79b86cba263a70e58baf2cb5bbc0187cf024
dragonlzm <dragonlzm123@gmail.com>
2022-07-22 20:13:29 -0700

    update for mask rcnn base line

--- configs/mask_rcnn/mask_rcnn_r50_fpn_1x_coco_2gpu.py
+++ configs/mask_rcnn/mask_rcnn_r50_fpn_1x_coco_2gpu.py
@@ -1,6 +1,8 @@
 _base_ = './mask_rcnn_r50_fpn_1x_coco_class_agnostic.py'
 
-optimizer = dict(lr=0.01)
+# the lr = 0.02 used for 8 gpu training, bs = 2*8
+# the lr 0.005 should match the bs = 2*2
+optimizer = dict(lr=0.005)
 
 model = dict(
     backbone=dict(
