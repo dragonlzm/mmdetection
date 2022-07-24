9e61046817c76c9dbcb76d2025b0cfdd021e0f51
dragonlzm <dragonlzm123@gmail.com>
2022-07-24 15:27:59 -0700

    update for shell script

--- tools/mask_rcnn_bs_test.sh
+++ tools/mask_rcnn_bs_test.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:v100:2
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=40GB
+#SBATCH --mem=80GB
 #SBATCH --time=48:00:00
 #SBATCH --account=nevatia_174
 
@@ -20,14 +20,14 @@ cd /project/nevatia_174/zhuoming/code/new_rpn/mmdetection
 
 
 # for 2*8
-# TRAIN_CONFIG="configs/mask_rcnn/mask_rcnn_r50_fpn_1x_coco_detectron_2x8.py"
-# WORK_DIR="/project/nevatia_174/zhuoming/detection/baseline/mask_rcnn_r50_fpn_1x_coco_detectron_2x8"
-# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
-# python -m torch.distributed.launch --nproc_per_node=2 \
-#     /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
-#     ${TRAIN_CONFIG} --launcher pytorch \
-#     --work-dir=${WORK_DIR} \
-#     #--resume-from=${WORK_DIR}/latest.pth
+TRAIN_CONFIG="configs/mask_rcnn/mask_rcnn_r50_fpn_1x_coco_detectron_2x8.py"
+WORK_DIR="/project/nevatia_174/zhuoming/detection/baseline/mask_rcnn_r50_fpn_1x_coco_detectron_2x8"
+PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=2 \
+    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+    ${TRAIN_CONFIG} --launcher pytorch \
+    --work-dir=${WORK_DIR} \
+    --resume-from=${WORK_DIR}/latest.pth
 
 # for 2*4
 # TRAIN_CONFIG="configs/mask_rcnn/mask_rcnn_r50_fpn_1x_coco_detectron_2x4.py"
@@ -37,7 +37,7 @@ cd /project/nevatia_174/zhuoming/code/new_rpn/mmdetection
 #     /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
 #     ${TRAIN_CONFIG} --launcher pytorch \
 #     --work-dir=${WORK_DIR} \
-#     #--resume-from=${WORK_DIR}/latest.pth
+#     --resume-from=${WORK_DIR}/latest.pth
 
 # # for 2*2
 # TRAIN_CONFIG="configs/mask_rcnn/mask_rcnn_r50_fpn_1x_coco_detectron_2x2.py"
@@ -51,14 +51,14 @@ cd /project/nevatia_174/zhuoming/code/new_rpn/mmdetection
 
 
 # for 2*8 base48
-TRAIN_CONFIG="configs/mask_rcnn/mask_rcnn_r50_fpn_1x_coco_detectron_2x8_base48.py"
-WORK_DIR="/project/nevatia_174/zhuoming/detection/baseline/mask_rcnn_r50_fpn_1x_coco_detectron_2x8_base48"
-PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=2 \
-    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
-    ${TRAIN_CONFIG} --launcher pytorch \
-    --work-dir=${WORK_DIR} \
-    #--resume-from=${WORK_DIR}/latest.pth
+# TRAIN_CONFIG="configs/mask_rcnn/mask_rcnn_r50_fpn_1x_coco_detectron_2x8_base48.py"
+# WORK_DIR="/project/nevatia_174/zhuoming/detection/baseline/mask_rcnn_r50_fpn_1x_coco_detectron_2x8_base48"
+# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+# python -m torch.distributed.launch --nproc_per_node=2 \
+#     /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+#     ${TRAIN_CONFIG} --launcher pytorch \
+#     --work-dir=${WORK_DIR} \
+#     --resume-from=${WORK_DIR}/latest.pth
 
 # for 2*4 base48
 # TRAIN_CONFIG="configs/mask_rcnn/mask_rcnn_r50_fpn_1x_coco_detectron_2x4_base48.py"
@@ -68,7 +68,7 @@ python -m torch.distributed.launch --nproc_per_node=2 \
 #     /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
 #     ${TRAIN_CONFIG} --launcher pytorch \
 #     --work-dir=${WORK_DIR} \
-#     #--resume-from=${WORK_DIR}/latest.pth
+#     --resume-from=${WORK_DIR}/latest.pth
 
 # # for 2*2 base48
 # TRAIN_CONFIG="configs/mask_rcnn/mask_rcnn_r50_fpn_1x_coco_detectron_2x2_base48.py"
