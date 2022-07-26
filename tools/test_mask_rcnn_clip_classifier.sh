3ce3247d2a4850f13a962276871658c9012c701c
dragonlzm <dragonlzm123@gmail.com>
2022-07-25 21:19:59 -0700

    update for shell script

--- tools/test_mask_rcnn_clip_classifier.sh
+++ tools/test_mask_rcnn_clip_classifier.sh
@@ -18,6 +18,10 @@ cd /project/nevatia_174/zhuoming/code/new_rpn/mmdetection
 #rm -rf ./data
 #ln -sf /project/nevatia_174/zhuoming/detection ./data
 
-bash tools/dist_test.sh configs/mask_rcnn_distill/mask_rcnn_clip_classifier_base48.py \
+# bash tools/dist_test.sh configs/mask_rcnn_distill/mask_rcnn_clip_classifier_base48.py \
+# data/grad_clip_check/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_base48_gn_10_200clipproposal/epoch_12.pth 2 \
+# --eval bbox segm --options jsonfile_prefix=data/mask_rcnn_clip_classifier/results_base48 
+
+bash tools/dist_test.sh configs/mask_rcnn_distill/mask_rcnn_clip_classifier_novel17.py \
 data/grad_clip_check/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_base48_gn_10_200clipproposal/epoch_12.pth 2 \
---eval bbox segm --options jsonfile_prefix=data/mask_rcnn_clip_classifier/results_base48 
+--eval bbox segm --options jsonfile_prefix=data/mask_rcnn_clip_classifier/results_novel17
