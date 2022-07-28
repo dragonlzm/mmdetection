f7698f53fb233573b4439de68a67b590585bf8f0
dragonlzm <dragonlzm123@gmail.com>
2022-07-28 14:57:22 -0700

    update for shell script

--- tools/mask_rcnn_distillation_detectron_setting.sh
+++ tools/mask_rcnn_distillation_detectron_setting.sh
@@ -18,44 +18,6 @@ cd /project/nevatia_174/zhuoming/code/new_rpn/mmdetection
 #rm -rf ./data
 #ln -sf /project/nevatia_174/zhuoming/detection ./data
 
-# 2*2 360k
-# COMBINE_METHOD='cat'
-# WORK_DIR="/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_detectron_2x2_360k_base48"
-# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
-# python -m torch.distributed.launch --nproc_per_node=2 \
-#     /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
-#     configs/mask_rcnn_distill/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_detectron_2x2_360k_base48.py --launcher pytorch \
-#     --work-dir=${WORK_DIR} \
-#     --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=1 optimizer_config.grad_clip.max_norm=10 \
-#     model.roi_head.bbox_head.combine_reg_and_cls_embedding=${COMBINE_METHOD} \
-#     #--resume-from=${WORK_DIR}/latest.pth
-
-# 2*2 360k double the regressor loss
-COMBINE_METHOD='cat'
-WORK_DIR="/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_detectron_2x2_360k_base48_2reg_loss"
-PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=2 \
-    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
-    configs/mask_rcnn_distill/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_detectron_2x2_360k_base48.py --launcher pytorch \
-    --work-dir=${WORK_DIR} \
-    --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=1 optimizer_config.grad_clip.max_norm=10 \
-    model.rpn_head.loss_bbox.loss_weight=2.0 model.roi_head.bbox_head.loss_bbox.loss_weight=2.0 \
-    model.roi_head.bbox_head.combine_reg_and_cls_embedding=${COMBINE_METHOD} \
-    #--resume-from=${WORK_DIR}/latest.pth
-
-# 2*2 360k double the RPN loss
-# COMBINE_METHOD='cat'
-# WORK_DIR="/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_detectron_2x2_360k_base48_2rpn_loss"
-# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
-# python -m torch.distributed.launch --nproc_per_node=2 \
-#     /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
-#     configs/mask_rcnn_distill/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_detectron_2x2_360k_base48.py --launcher pytorch \
-#     --work-dir=${WORK_DIR} \
-#     --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=1 optimizer_config.grad_clip.max_norm=10 \
-#     model.rpn_head.loss_bbox.loss_weight=2.0 model.rpn_head.loss_cls.loss_weight=2.0 \
-#     model.roi_head.bbox_head.combine_reg_and_cls_embedding=${COMBINE_METHOD} \
-#     #--resume-from=${WORK_DIR}/latest.pth
-
 # 2*4 180k
 # COMBINE_METHOD='cat'
 # WORK_DIR="/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_detectron_2x4_180k_base48"
