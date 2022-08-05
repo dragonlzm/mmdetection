dfc0ae80fcc51b0d687ef0e1dfbba02d46e657f1
dragonlzm <dragonlzm123@gmail.com>
2022-08-04 20:31:23 -0700

    update for shell script

--- tools/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_base48_copy_and_paste.sh
+++ tools/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_base48_copy_and_paste.sh
@@ -29,7 +29,7 @@ python -m torch.distributed.launch --nproc_per_node=2 \
     --work-dir=${WORK_DIR} \
     --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=1 optimizer_config.grad_clip.max_norm=10 \
     model.roi_head.bbox_head.combine_reg_and_cls_embedding=${COMBINE_METHOD} \
-    #--resume-from=${WORK_DIR}/latest.pth
+    --resume-from=${WORK_DIR}/latest.pth
 
 
 # test the model
@@ -65,3 +65,54 @@ ${WORK_DIR}/${CHECKPOINT_NAME} 2 --eval bbox segm \
 --cfg-options data.test.eval_filter_empty_gt=False data.test.ann_file=data/coco/annotations/instances_val2017_65cates.json \
 model.roi_head.bbox_head.combine_reg_and_cls_embedding=${COMBINE_METHOD} model.roi_head.bbox_head.reg_with_cls_embedding=True \
 #model.roi_head.bbox_head.reg_with_mlp=True
+
+
+
+### the following should be delete ###
+
+# 200 clip proposal filpping(reg with class embedding, cat, distillation weight=0.5, contrastive weight=1)
+COMBINE_METHOD='cat'
+WORK_DIR="/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_distillation_w05_contrast_w1"
+PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=2 \
+    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+    configs/mask_rcnn_distill/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_base48_200clip_pro_reg_with_embedding.py --launcher pytorch \
+    --work-dir=${WORK_DIR} \
+    --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=1 optimizer_config.grad_clip.max_norm=10 \
+    model.train_cfg.rcnn.use_contrast_distill=True  model.train_cfg.rcnn.contrastive_weight=1 \
+    model.roi_head.bbox_head.combine_reg_and_cls_embedding=${COMBINE_METHOD} \
+    #--resume-from=${WORK_DIR}/latest.pth
+
+
+# test the model
+#CHECKPOINT_NAME="epoch_12.pth"
+#CHECKPOINT_NAME="epoch_24.pth"
+CHECKPOINT_NAME="latest.pth"
+
+bash tools/dist_test.sh configs/mask_rcnn_distill/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_base48.py \
+${WORK_DIR}/${CHECKPOINT_NAME} 2 --eval bbox segm \
+--eval-options jsonfile_prefix=${WORK_DIR}/base_results \
+--cfg-options data.test.eval_filter_empty_gt=False data.test.ann_file=data/coco/annotations/instances_val2017_65cates.json \
+model.roi_head.bbox_head.combine_reg_and_cls_embedding=${COMBINE_METHOD} model.roi_head.bbox_head.reg_with_cls_embedding=True \
+
+
+bash tools/dist_test.sh configs/mask_rcnn_distill/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_novel17.py \
+${WORK_DIR}/${CHECKPOINT_NAME} 2 --eval bbox segm \
+--eval-options jsonfile_prefix=${WORK_DIR}/novel_results \
+--cfg-options data.test.eval_filter_empty_gt=False data.test.ann_file=data/coco/annotations/instances_val2017_65cates.json \
+model.roi_head.bbox_head.combine_reg_and_cls_embedding=${COMBINE_METHOD} model.roi_head.bbox_head.reg_with_cls_embedding=True \
+
+
+bash tools/dist_test.sh configs/mask_rcnn_distill/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_novel17.py \
+${WORK_DIR}/${CHECKPOINT_NAME} 2 --eval bbox segm \
+--eval-options jsonfile_prefix=${WORK_DIR}/novel_results_trick \
+--cfg-options model.roi_head.bbox_head.filter_base_cate=data/embeddings/base_finetuned_48cates.pt data.test.eval_filter_empty_gt=False \
+data.test.ann_file=data/coco/annotations/instances_val2017_65cates.json \
+model.roi_head.bbox_head.combine_reg_and_cls_embedding=${COMBINE_METHOD} model.roi_head.bbox_head.reg_with_cls_embedding=True \
+
+
+bash tools/dist_test.sh configs/mask_rcnn_distill/mask_rcnn_with_base48_tuned_clip_feat_r50_fpn_1x_coco_bn65.py \
+${WORK_DIR}/${CHECKPOINT_NAME} 2 --eval bbox segm \
+--eval-options jsonfile_prefix=${WORK_DIR}/base_and_novel \
+--cfg-options data.test.eval_filter_empty_gt=False data.test.ann_file=data/coco/annotations/instances_val2017_65cates.json \
+model.roi_head.bbox_head.combine_reg_and_cls_embedding=${COMBINE_METHOD} model.roi_head.bbox_head.reg_with_cls_embedding=True \
