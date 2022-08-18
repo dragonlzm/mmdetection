f8684cda9f3a03fa68c10aad1611cea5cd4982ef
dragonlzm <dragonlzm123@gmail.com>
2022-08-18 09:58:46 -0700

    update for model config

--- mmdet/models/detectors/faster_rcnn_with_clip_feat.py
+++ mmdet/models/detectors/faster_rcnn_with_clip_feat.py
@@ -1,25 +1,31 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from ..builder import DETECTORS
-from .two_stage import MaskRCNNWithCLIPFeat
+from .mask_rcnn_with_clip_feat import MaskRCNNWithCLIPFeat
 
 
 @DETECTORS.register_module()
 class FasterRCNNWithCLIPFeat(MaskRCNNWithCLIPFeat):
     def __init__(self,
                  backbone,
-                 rpn_head,
-                 roi_head,
-                 train_cfg,
-                 test_cfg,
                  neck=None,
+                 rpn_head=None,
+                 unknow_rpn_head=None,
+                 roi_head=None,
+                 rand_bboxes_num=20,
+                 train_cfg=None,
+                 test_cfg=None,
                  pretrained=None,
-                 init_cfg=None):
+                 init_cfg=None,
+                 vit_backbone_cfg=None):
         super(FasterRCNNWithCLIPFeat, self).__init__(
-            backbone=backbone,
-            neck=neck,
-            rpn_head=rpn_head,
-            roi_head=roi_head,
-            train_cfg=train_cfg,
-            test_cfg=test_cfg,
-            pretrained=pretrained,
-            init_cfg=init_cfg)
+                backbone,
+                neck=neck,
+                rpn_head=rpn_head,
+                unknow_rpn_head=unknow_rpn_head,
+                roi_head=roi_head,
+                rand_bboxes_num=rand_bboxes_num,
+                train_cfg=train_cfg,
+                test_cfg=test_cfg,
+                pretrained=pretrained,
+                init_cfg=init_cfg,
+                vit_backbone_cfg=vit_backbone_cfg)
