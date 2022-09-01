2cbd76b62316f4cbcd1db9abe5a7e595841fe4b8
dragonlzm <dragonlzm123@gmail.com>
2022-09-01 13:04:38 -0700

    update for shell script

--- tools/cluster_script/finetune_clip/cls_finetuner_clip_lvis_base_train.sh
+++ tools/cluster_script/finetune_clip/cls_finetuner_clip_lvis_base_train.sh
@@ -98,62 +98,15 @@ cd /project/nevatia_174/zhuoming/code/new_rpn/mmdetection
 
 ### longer training schedule exp
 # vision only(with lr drop)
-START_FROM="data/exp_res/cls_finetuner_clip_lvis_base_train_over_sample"
-WORK_DIR="data/exp_res/cls_finetuner_clip_lvis_base_train_over_sample_v1"
-PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=2 \
-    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
-    configs/cls_finetuner/cls_finetuner_clip_lvis_base_train_over_sample.py --launcher pytorch \
-    --work-dir=${WORK_DIR} \
-    --cfg-options lr_config.step=[12,] runner.max_epochs=18 \
-    --resume-from=${START_FROM}/latest.pth
-
-bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_lvis_base_train.py \
-${WORK_DIR}/epoch_12.pth 2 --eval=gt_acc \
---options jsonfile_prefix=${WORK_DIR}/base_results
-
-bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_lvis_novel_train.py \
-${WORK_DIR}/epoch_12.pth 2 --eval=gt_acc \
---options jsonfile_prefix=${WORK_DIR}/novel_results
-
-bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_full_lvis.py \
-${WORK_DIR}/epoch_12.pth 2 --eval=gt_acc \
---options jsonfile_prefix=${WORK_DIR}/all_results
-
-# vision only(without lr drop)
-START_FROM="data/exp_res/cls_finetuner_clip_lvis_base_train_over_sample"
-WORK_DIR="data/exp_res/cls_finetuner_clip_lvis_base_train_over_sample_v2"
-PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=2 \
-    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
-    configs/cls_finetuner/cls_finetuner_clip_lvis_base_train_over_sample.py --launcher pytorch \
-    --work-dir=${WORK_DIR} \
-    --cfg-options runner.max_epochs=18 \
-    --resume-from=${START_FROM}/latest.pth
-
-bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_lvis_base_train.py \
-${WORK_DIR}/epoch_12.pth 2 --eval=gt_acc \
---options jsonfile_prefix=${WORK_DIR}/base_results
-
-bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_lvis_novel_train.py \
-${WORK_DIR}/epoch_12.pth 2 --eval=gt_acc \
---options jsonfile_prefix=${WORK_DIR}/novel_results
-
-bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_full_lvis.py \
-${WORK_DIR}/epoch_12.pth 2 --eval=gt_acc \
---options jsonfile_prefix=${WORK_DIR}/all_results
-
-
-
-# bash tools/new_dist_train.sh configs/cls_finetuner/cls_finetuner_clip_lvis_base_train_over_sample.py 2 \
-# data/exp_res/cls_finetuner_clip_lvis_base_train_over_sample ./data \
-# --cfg-options lr_config.step=[12,] runner.max_epochs=18 \
-# --resume-from=data/exp_res/cls_finetuner_clip_lvis_base_train_over_sample/epoch_12.pth
-
-# bash tools/new_dist_train_23.sh configs/cls_finetuner/cls_finetuner_clip_lvis_base_train_gt_only.py 2 \
-# data/exp_res/cls_finetuner_clip_lvis_base_train_gt_and_100_rand_embedding ./data \
-# --cfg-options lr_config.step=[12,] runner.max_epochs=18 model.rpn_head.use_rand_name=100 \
-# --resume-from=data/exp_res/cls_finetuner_clip_lvis_base_train_gt_and_100_rand_embedding/epoch_12.pth
+# START_FROM="data/exp_res/cls_finetuner_clip_lvis_base_train_over_sample"
+# WORK_DIR="data/exp_res/cls_finetuner_clip_lvis_base_train_over_sample_v1"
+# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+# python -m torch.distributed.launch --nproc_per_node=2 \
+#     /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+#     configs/cls_finetuner/cls_finetuner_clip_lvis_base_train_over_sample.py --launcher pytorch \
+#     --work-dir=${WORK_DIR} \
+#     --cfg-options lr_config.step=[12,] runner.max_epochs=18 \
+#     --resume-from=${START_FROM}/latest.pth
 
 # bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_lvis_base_train.py \
 # ${WORK_DIR}/epoch_12.pth 2 --eval=gt_acc \
@@ -166,3 +119,72 @@ ${WORK_DIR}/epoch_12.pth 2 --eval=gt_acc \
 # bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_full_lvis.py \
 # ${WORK_DIR}/epoch_12.pth 2 --eval=gt_acc \
 # --options jsonfile_prefix=${WORK_DIR}/all_results
+
+# # vision only(without lr drop)
+# START_FROM="data/exp_res/cls_finetuner_clip_lvis_base_train_over_sample"
+# WORK_DIR="data/exp_res/cls_finetuner_clip_lvis_base_train_over_sample_v2"
+# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+# python -m torch.distributed.launch --nproc_per_node=2 \
+#     /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+#     configs/cls_finetuner/cls_finetuner_clip_lvis_base_train_over_sample.py --launcher pytorch \
+#     --work-dir=${WORK_DIR} \
+#     --cfg-options runner.max_epochs=18 \
+#     --resume-from=${START_FROM}/latest.pth
+
+# bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_lvis_base_train.py \
+# ${WORK_DIR}/epoch_12.pth 2 --eval=gt_acc \
+# --options jsonfile_prefix=${WORK_DIR}/base_results
+
+# bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_lvis_novel_train.py \
+# ${WORK_DIR}/epoch_12.pth 2 --eval=gt_acc \
+# --options jsonfile_prefix=${WORK_DIR}/novel_results
+
+# bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_full_lvis.py \
+# ${WORK_DIR}/epoch_12.pth 2 --eval=gt_acc \
+# --options jsonfile_prefix=${WORK_DIR}/all_results
+
+# random 100 embedding, with lr drop
+START_FROM="data/exp_res/cls_finetuner_clip_lvis_base_train_gt_and_100_rand_embedding"
+WORK_DIR="data/exp_res/cls_finetuner_clip_lvis_base_train_gt_and_100_rand_embedding_v1"
+PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=2 \
+    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+    configs/cls_finetuner/cls_finetuner_clip_lvis_base_train_gt_only.py --launcher pytorch \
+    --work-dir=${WORK_DIR} \
+    --cfg-options lr_config.step=[12,] runner.max_epochs=18 model.rpn_head.use_rand_name=100 \
+    --resume-from=${START_FROM}/latest.pth
+
+bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_lvis_base_train.py \
+${WORK_DIR}/epoch_12.pth 2 --eval=gt_acc \
+--options jsonfile_prefix=${WORK_DIR}/base_results
+
+bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_lvis_novel_train.py \
+${WORK_DIR}/epoch_12.pth 2 --eval=gt_acc \
+--options jsonfile_prefix=${WORK_DIR}/novel_results
+
+bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_full_lvis.py \
+${WORK_DIR}/epoch_12.pth 2 --eval=gt_acc \
+--options jsonfile_prefix=${WORK_DIR}/all_results
+
+# random 100 embedding, wo lr drop
+START_FROM="data/exp_res/cls_finetuner_clip_lvis_base_train_gt_and_100_rand_embedding"
+WORK_DIR="data/exp_res/cls_finetuner_clip_lvis_base_train_gt_and_100_rand_embedding_v2"
+PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=2 \
+    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+    configs/cls_finetuner/cls_finetuner_clip_lvis_base_train_gt_only.py --launcher pytorch \
+    --work-dir=${WORK_DIR} \
+    --cfg-options runner.max_epochs=18 model.rpn_head.use_rand_name=100 \
+    --resume-from=${START_FROM}/latest.pth
+
+bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_lvis_base_train.py \
+${WORK_DIR}/epoch_12.pth 2 --eval=gt_acc \
+--options jsonfile_prefix=${WORK_DIR}/base_results
+
+bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_lvis_novel_train.py \
+${WORK_DIR}/epoch_12.pth 2 --eval=gt_acc \
+--options jsonfile_prefix=${WORK_DIR}/novel_results
+
+bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_full_lvis.py \
+${WORK_DIR}/epoch_12.pth 2 --eval=gt_acc \
+--options jsonfile_prefix=${WORK_DIR}/all_results
