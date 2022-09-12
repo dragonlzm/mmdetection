d5310e6d64696527003504b9dba0fc3086a7b295
dragonlzm <dragonlzm123@gmail.com>
2022-09-12 13:15:16 -0700

    update for config

--- tools/interactive_session.sh
+++ tools/interactive_session.sh
@@ -1,1 +1,2 @@
 salloc --time=12:00:00 --partition=gpu --gres=gpu:v100:2 --account=nevatia_174 --ntasks=1 --cpus-per-task=4 --mem=80GB
+salloc --time=12:00:00 --partition=gpu --gres=gpu:p100:2 --account=nevatia_174 --ntasks=1 --cpus-per-task=4 --mem=80GB
