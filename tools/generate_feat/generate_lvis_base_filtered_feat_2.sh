fc0f87604e0a1a285021cae97d836a9666abf6a3
dragonlzm <dragonlzm123@gmail.com>
2022-09-11 22:00:33 -0700

    update for shell script

--- tools/generate_feat/generate_lvis_base_filtered_feat_2.sh
+++ tools/generate_feat/generate_lvis_base_filtered_feat_2.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:v100:2
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=30GB
+#SBATCH --mem=80GB
 #SBATCH --time=48:00:00
 #SBATCH --account=nevatia_174
 
@@ -28,103 +28,103 @@ CONFIG_FILE="configs/cls_finetuner/cls_finetuner_clip_full_lvis.py"
 BBOX_SAVE_PATH_ROOT="data/lvis_v1/clip_proposal/lvis_32_32_512"
 FEAT_SAVE_PATH_ROOT="data/lvis_v1/clip_proposal_feat/lvis_base_finetuned"
 
-# 7
-PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=2 \
-   /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/test.py \
-   ${CONFIG_FILE} \
-   ${CHECKPOINT} \
-   --launcher pytorch --eval=gt_acc \
-   --eval-options jsonfile_prefix=${FEAT_SAVE_PATH_ROOT}/extract_feat \
-   --cfg-options data.test.ann_file=data/lvis_v1/annotations/lvis_v1_train_48000_56000.json \
-   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=${FEAT_SAVE_PATH_ROOT} \
-   model.test_cfg.use_pregenerated_proposal=${BBOX_SAVE_PATH_ROOT} \
-   model.test_cfg.num_of_rand_bboxes=1000 model.test_cfg.filter_clip_proposal_base_on_cates=True \
-   model.test_cfg.save_cates_and_conf=True
+# # 7
+# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+# python -m torch.distributed.launch --nproc_per_node=2 \
+#    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/test.py \
+#    ${CONFIG_FILE} \
+#    ${CHECKPOINT} \
+#    --launcher pytorch --eval=gt_acc \
+#    --eval-options jsonfile_prefix=${FEAT_SAVE_PATH_ROOT}/extract_feat \
+#    --cfg-options data.test.ann_file=data/lvis_v1/annotations/lvis_v1_train_48000_56000.json \
+#    model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=${FEAT_SAVE_PATH_ROOT} \
+#    model.test_cfg.use_pregenerated_proposal=${BBOX_SAVE_PATH_ROOT} \
+#    model.test_cfg.num_of_rand_bboxes=1000 model.test_cfg.filter_clip_proposal_base_on_cates=True \
+#    model.test_cfg.save_cates_and_conf=True
 
-# 8
-PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=2 \
-   /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/test.py \
-   ${CONFIG_FILE} \
-   ${CHECKPOINT} \
-   --launcher pytorch --eval=gt_acc \
-   --eval-options jsonfile_prefix=${FEAT_SAVE_PATH_ROOT}/extract_feat \
-   --cfg-options data.test.ann_file=data/lvis_v1/annotations/lvis_v1_train_56000_64000.json \
-   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=${FEAT_SAVE_PATH_ROOT} \
-   model.test_cfg.use_pregenerated_proposal=${BBOX_SAVE_PATH_ROOT} \
-   model.test_cfg.num_of_rand_bboxes=1000 model.test_cfg.filter_clip_proposal_base_on_cates=True \
-   model.test_cfg.save_cates_and_conf=True
+# # 8
+# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+# python -m torch.distributed.launch --nproc_per_node=2 \
+#    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/test.py \
+#    ${CONFIG_FILE} \
+#    ${CHECKPOINT} \
+#    --launcher pytorch --eval=gt_acc \
+#    --eval-options jsonfile_prefix=${FEAT_SAVE_PATH_ROOT}/extract_feat \
+#    --cfg-options data.test.ann_file=data/lvis_v1/annotations/lvis_v1_train_56000_64000.json \
+#    model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=${FEAT_SAVE_PATH_ROOT} \
+#    model.test_cfg.use_pregenerated_proposal=${BBOX_SAVE_PATH_ROOT} \
+#    model.test_cfg.num_of_rand_bboxes=1000 model.test_cfg.filter_clip_proposal_base_on_cates=True \
+#    model.test_cfg.save_cates_and_conf=True
 
-# 9
-PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=2 \
-   /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/test.py \
-   ${CONFIG_FILE} \
-   ${CHECKPOINT} \
-   --launcher pytorch --eval=gt_acc \
-   --eval-options jsonfile_prefix=${FEAT_SAVE_PATH_ROOT}/extract_feat \
-   --cfg-options data.test.ann_file=data/lvis_v1/annotations/lvis_v1_train_64000_72000.json \
-   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=${FEAT_SAVE_PATH_ROOT} \
-   model.test_cfg.use_pregenerated_proposal=${BBOX_SAVE_PATH_ROOT} \
-   model.test_cfg.num_of_rand_bboxes=1000 model.test_cfg.filter_clip_proposal_base_on_cates=True \
-   model.test_cfg.save_cates_and_conf=True
+# # 9
+# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+# python -m torch.distributed.launch --nproc_per_node=2 \
+#    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/test.py \
+#    ${CONFIG_FILE} \
+#    ${CHECKPOINT} \
+#    --launcher pytorch --eval=gt_acc \
+#    --eval-options jsonfile_prefix=${FEAT_SAVE_PATH_ROOT}/extract_feat \
+#    --cfg-options data.test.ann_file=data/lvis_v1/annotations/lvis_v1_train_64000_72000.json \
+#    model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=${FEAT_SAVE_PATH_ROOT} \
+#    model.test_cfg.use_pregenerated_proposal=${BBOX_SAVE_PATH_ROOT} \
+#    model.test_cfg.num_of_rand_bboxes=1000 model.test_cfg.filter_clip_proposal_base_on_cates=True \
+#    model.test_cfg.save_cates_and_conf=True
 
-# 10
-PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=2 \
-   /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/test.py \
-   ${CONFIG_FILE} \
-   ${CHECKPOINT} \
-   --launcher pytorch --eval=gt_acc \
-   --eval-options jsonfile_prefix=${FEAT_SAVE_PATH_ROOT}/extract_feat \
-   --cfg-options data.test.ann_file=data/lvis_v1/annotations/lvis_v1_train_72000_80000.json \
-   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=${FEAT_SAVE_PATH_ROOT} \
-   model.test_cfg.use_pregenerated_proposal=${BBOX_SAVE_PATH_ROOT} \
-   model.test_cfg.num_of_rand_bboxes=1000 model.test_cfg.filter_clip_proposal_base_on_cates=True \
-   model.test_cfg.save_cates_and_conf=True
+# # 10
+# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+# python -m torch.distributed.launch --nproc_per_node=2 \
+#    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/test.py \
+#    ${CONFIG_FILE} \
+#    ${CHECKPOINT} \
+#    --launcher pytorch --eval=gt_acc \
+#    --eval-options jsonfile_prefix=${FEAT_SAVE_PATH_ROOT}/extract_feat \
+#    --cfg-options data.test.ann_file=data/lvis_v1/annotations/lvis_v1_train_72000_80000.json \
+#    model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=${FEAT_SAVE_PATH_ROOT} \
+#    model.test_cfg.use_pregenerated_proposal=${BBOX_SAVE_PATH_ROOT} \
+#    model.test_cfg.num_of_rand_bboxes=1000 model.test_cfg.filter_clip_proposal_base_on_cates=True \
+#    model.test_cfg.save_cates_and_conf=True
 
-# 11
-PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=2 \
-   /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/test.py \
-   ${CONFIG_FILE} \
-   ${CHECKPOINT} \
-   --launcher pytorch --eval=gt_acc \
-   --eval-options jsonfile_prefix=${FEAT_SAVE_PATH_ROOT}/extract_feat \
-   --cfg-options data.test.ann_file=data/lvis_v1/annotations/lvis_v1_train_80000_88000.json \
-   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=${FEAT_SAVE_PATH_ROOT} \
-   model.test_cfg.use_pregenerated_proposal=${BBOX_SAVE_PATH_ROOT} \
-   model.test_cfg.num_of_rand_bboxes=1000 model.test_cfg.filter_clip_proposal_base_on_cates=True \
-   model.test_cfg.save_cates_and_conf=True
+# # 11
+# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+# python -m torch.distributed.launch --nproc_per_node=2 \
+#    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/test.py \
+#    ${CONFIG_FILE} \
+#    ${CHECKPOINT} \
+#    --launcher pytorch --eval=gt_acc \
+#    --eval-options jsonfile_prefix=${FEAT_SAVE_PATH_ROOT}/extract_feat \
+#    --cfg-options data.test.ann_file=data/lvis_v1/annotations/lvis_v1_train_80000_88000.json \
+#    model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=${FEAT_SAVE_PATH_ROOT} \
+#    model.test_cfg.use_pregenerated_proposal=${BBOX_SAVE_PATH_ROOT} \
+#    model.test_cfg.num_of_rand_bboxes=1000 model.test_cfg.filter_clip_proposal_base_on_cates=True \
+#    model.test_cfg.save_cates_and_conf=True
 
-# 12
-PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=2 \
-   /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/test.py \
-   ${CONFIG_FILE} \
-   ${CHECKPOINT} \
-   --launcher pytorch --eval=gt_acc \
-   --eval-options jsonfile_prefix=${FEAT_SAVE_PATH_ROOT}/extract_feat \
-   --cfg-options data.test.ann_file=data/lvis_v1/annotations/lvis_v1_train_88000_96000.json \
-   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=${FEAT_SAVE_PATH_ROOT} \
-   model.test_cfg.use_pregenerated_proposal=${BBOX_SAVE_PATH_ROOT} \
-   model.test_cfg.num_of_rand_bboxes=1000 model.test_cfg.filter_clip_proposal_base_on_cates=True \
-   model.test_cfg.save_cates_and_conf=True
+# # 12
+# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+# python -m torch.distributed.launch --nproc_per_node=2 \
+#    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/test.py \
+#    ${CONFIG_FILE} \
+#    ${CHECKPOINT} \
+#    --launcher pytorch --eval=gt_acc \
+#    --eval-options jsonfile_prefix=${FEAT_SAVE_PATH_ROOT}/extract_feat \
+#    --cfg-options data.test.ann_file=data/lvis_v1/annotations/lvis_v1_train_88000_96000.json \
+#    model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=${FEAT_SAVE_PATH_ROOT} \
+#    model.test_cfg.use_pregenerated_proposal=${BBOX_SAVE_PATH_ROOT} \
+#    model.test_cfg.num_of_rand_bboxes=1000 model.test_cfg.filter_clip_proposal_base_on_cates=True \
+#    model.test_cfg.save_cates_and_conf=True
 
-# 13
-PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=2 \
-   /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/test.py \
-   ${CONFIG_FILE} \
-   ${CHECKPOINT} \
-   --launcher pytorch --eval=gt_acc \
-   --eval-options jsonfile_prefix=${FEAT_SAVE_PATH_ROOT}/extract_feat \
-   --cfg-options data.test.ann_file=data/lvis_v1/annotations/lvis_v1_train_96000_104000.json \
-   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=${FEAT_SAVE_PATH_ROOT} \
-   model.test_cfg.use_pregenerated_proposal=${BBOX_SAVE_PATH_ROOT} \
-   model.test_cfg.num_of_rand_bboxes=1000 model.test_cfg.filter_clip_proposal_base_on_cates=True \
-   model.test_cfg.save_cates_and_conf=True
+# # 13
+# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+# python -m torch.distributed.launch --nproc_per_node=2 \
+#    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/test.py \
+#    ${CONFIG_FILE} \
+#    ${CHECKPOINT} \
+#    --launcher pytorch --eval=gt_acc \
+#    --eval-options jsonfile_prefix=${FEAT_SAVE_PATH_ROOT}/extract_feat \
+#    --cfg-options data.test.ann_file=data/lvis_v1/annotations/lvis_v1_train_96000_104000.json \
+#    model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=${FEAT_SAVE_PATH_ROOT} \
+#    model.test_cfg.use_pregenerated_proposal=${BBOX_SAVE_PATH_ROOT} \
+#    model.test_cfg.num_of_rand_bboxes=1000 model.test_cfg.filter_clip_proposal_base_on_cates=True \
+#    model.test_cfg.save_cates_and_conf=True
 
 # all
 PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
