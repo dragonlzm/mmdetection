ad4fde5469ad8d696356ca2ff50ff37f34e54d42
dragonlzm <dragonlzm123@gmail.com>
2022-09-12 20:44:14 -0700

    update for cumulate grad

--- configs/mask_rcnn_distill/mask_rcnn_distillation_with_transformer_per_base_filtered_clip_proposal_weight_base48_paramwise_grad_clip.py
+++ configs/mask_rcnn_distill/mask_rcnn_distillation_with_transformer_per_base_filtered_clip_proposal_weight_base48_paramwise_grad_clip.py
@@ -1,7 +1,7 @@
 _base_ = './mask_rcnn_distillation_with_transformer_per_base_filtered_clip_proposal_weight_base48.py'
 
-# using total batchsize 8
-optimizer = dict(lr=0.01)
+# using total batchsize 16, by using the ParamWiseGradientCumulativeOptimizerHook
+optimizer = dict(lr=0.02)
 data = dict(
     samples_per_gpu=2,
     workers_per_gpu=2)
@@ -9,6 +9,7 @@ data = dict(
 # regression with embedding, base filtered proposal, per distillation bbox weight
 # become default setting in here
 optimizer_config = dict(_delete_=True, 
-                        type='ParamWiseOptimizerHook', 
+                        type='ParamWiseGradientCumulativeOptimizerHook', 
+                        cumulative_iters=2,
                         grad_clip=dict(encoder=dict(max_norm=0.01, norm_type=2) , 
                                        other=dict(max_norm=10, norm_type=2)))
