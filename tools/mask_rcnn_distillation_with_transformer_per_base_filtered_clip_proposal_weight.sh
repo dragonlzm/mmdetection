27390502f22239a8658bc9c4c381341588b7e8eb
dragonlzm <dragonlzm123@gmail.com>
2022-09-16 16:18:06 -0700

    Update mask_rcnn_distillation_with_transformer_per_base_filtered_clip_proposal_weight.sh

--- tools/mask_rcnn_distillation_with_transformer_per_base_filtered_clip_proposal_weight.sh
+++ tools/mask_rcnn_distillation_with_transformer_per_base_filtered_clip_proposal_weight.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --partition=gpu 
-#SBATCH --gres=gpu:v100:2
+#SBATCH --gres=gpu:p100:2
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=40GB
@@ -72,18 +72,18 @@ cd /project/nevatia_174/zhuoming/code/new_rpn/mmdetection
 #     #--seed=43 --deterministic \
 
 # base_filtered gt weight = 1, v2, use_proposal_for_distill, tranformer_multiplier=0.005
-ADDITIONAL_CONFIG="model.roi_head.type='StandardRoIHeadDistillWithTransformerV2'"
-WORK_DIR="/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_distillation_with_transformerv2_dist_with_prop_per_base_filtered_clip_proposal_weight_base48_tm005"
-PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=2 \
-    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
-    configs/mask_rcnn_distill/mask_rcnn_distillation_with_transformer_per_base_filtered_clip_proposal_weight_base48.py --launcher pytorch \
-    --work-dir=${WORK_DIR} \
-    --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=1 optimizer_config.grad_clip.max_norm=10 \
-    ${ADDITIONAL_CONFIG} \
-    model.train_cfg.rcnn.use_proposal_for_distill=True optimizer.tranformer_multiplier=0.005 \
-    #--resume-from=${WORK_DIR}/latest.pth
-    #--seed=43 --deterministic \
+# ADDITIONAL_CONFIG="model.roi_head.type='StandardRoIHeadDistillWithTransformerV2'"
+# WORK_DIR="/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_distillation_with_transformerv2_dist_with_prop_per_base_filtered_clip_proposal_weight_base48_tm005"
+# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+# python -m torch.distributed.launch --nproc_per_node=2 \
+#     /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+#     configs/mask_rcnn_distill/mask_rcnn_distillation_with_transformer_per_base_filtered_clip_proposal_weight_base48.py --launcher pytorch \
+#     --work-dir=${WORK_DIR} \
+#     --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=1 optimizer_config.grad_clip.max_norm=10 \
+#     ${ADDITIONAL_CONFIG} \
+#     model.train_cfg.rcnn.use_proposal_for_distill=True optimizer.tranformer_multiplier=0.005 \
+#     #--resume-from=${WORK_DIR}/latest.pth
+#     #--seed=43 --deterministic \
 
 ### update grad clip
 # base_filtered gt weight = 1, v2, use_proposal_for_distill, tranformer_multiplier=0.005, whole model 0.1
@@ -113,6 +113,20 @@ python -m torch.distributed.launch --nproc_per_node=2 \
 #     model.train_cfg.rcnn.use_proposal_for_distill=True optimizer.tranformer_multiplier=0.005 optimizer_config.grad_clip.max_norm=0.01 \
 #     #--resume-from=${WORK_DIR}/latest.pth
 #     #--seed=43 --deterministic \
+
+# base_filtered gt weight = 1, v2, use_proposal_for_distill, tranformer_multiplier=0.005
+ADDITIONAL_CONFIG="model.roi_head.type='StandardRoIHeadDistillWithTransformerV2'"
+WORK_DIR="/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_distillation_with_transformer_bs4_lr0005"
+PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=2 \
+    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+    configs/mask_rcnn_distill/mask_rcnn_distillation_with_transformer_per_base_filtered_clip_proposal_weight_base48_paramwise_grad_clip.py --launcher pytorch \
+    --work-dir=${WORK_DIR} \
+    --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=1 optimizer.lr=0.005 \
+    ${ADDITIONAL_CONFIG} optimizer_config.cumulative_iters=1 \
+    model.train_cfg.rcnn.use_proposal_for_distill=True \
+    #--resume-from=${WORK_DIR}/latest.pth
+    #--seed=43 --deterministic \
 
 # test the model
 #CHECKPOINT_NAME="epoch_12.pth"
