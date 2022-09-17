a22a6194c12266a25932f1d6acbcd1a93a455f8b
dragonlzm <dragonlzm123@gmail.com>
2022-09-16 23:29:59 -0700

    update for config

---
+++ configs/mask_rcnn_distill/mask_rcnn_distillation_with_transformer_per_base_filtered_clip_proposal_weight_base48_paramwise_grad_clip_exp.py
@@ -0,0 +1,11 @@
+_base_ = './mask_rcnn_distillation_with_transformer_per_base_filtered_clip_proposal_weight_base48_paramwise_grad_clip.py'
+
+# using total batchsize 16, by using the ParamWiseGradientCumulativeOptimizerHook
+optimizer = dict(lr=0.02)
+
+# regression with embedding, base filtered proposal, per distillation bbox weight
+# become default setting in here
+optimizer_config = dict(_delete_=True, 
+                        type='ParamWiseGradientCumulativeOptimizerHook', 
+                        cumulative_iters=2,
+                        grad_clip=dict(encoder=dict(max_norm=0.01, norm_type=2)))
