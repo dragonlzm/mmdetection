1ba8282364f414d6fa208fcb2b346db2bb3c0353
dragonlzm <dragonlzm123@gmail.com>
2022-09-19 09:15:37 -0700

    update for config

--- configs/mask_rcnn_distill/mask_rcnn_distillation_with_transformer_per_base_filtered_clip_proposal_weight_base48_paramwise_grad_clip_exp1.py
+++
@@ -1,9 +0,0 @@
-_base_ = './mask_rcnn_distillation_with_transformer_per_base_filtered_clip_proposal_weight_base48_paramwise_grad_clip_exp.py'
-
-# using total batchsize 16, by using the ParamWiseGradientCumulativeOptimizerHook
-optimizer = dict(lr=0.005)
-
-optimizer_config = dict(_delete_=True, 
-                        type='ParamWiseGradientCumulativeOptimizerHook', 
-                        cumulative_iters=2,
-                        grad_clip=dict(encoder=dict(max_norm=0.01, norm_type=2)))
