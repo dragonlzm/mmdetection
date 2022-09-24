cd125c9c54d8acdaf2f360a1ea890bd867105576
dragonlzm <dragonlzm123@gmail.com>
2022-09-24 09:48:24 -0700

    update for shell script

--- tools/mask_rcnn_distillation_lvis.sh
+++ tools/mask_rcnn_distillation_lvis.sh
@@ -18,6 +18,7 @@ cd /project/nevatia_174/zhuoming/code/new_rpn/mmdetection
 #rm -rf ./data
 #ln -sf /project/nevatia_174/zhuoming/detection ./data
 
+#### 2x setting experiment
 # base_filtered gt weight = 1 
 # WORK_DIR="/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_distillation_lvis_base"
 # PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
@@ -27,7 +28,6 @@ cd /project/nevatia_174/zhuoming/code/new_rpn/mmdetection
 #     --work-dir=${WORK_DIR} \
 #     --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=1 optimizer_config.grad_clip.max_norm=10 \
 #     #--resume-from=${WORK_DIR}/latest.pth
-
 
 # base_filtered gt weight = 1, 2x regression weight
 ADDITIONAL_CONFIG="model.rpn_head.loss_bbox.loss_weight=2.0 model.roi_head.bbox_head.loss_bbox.loss_weight=2.0"
@@ -49,8 +49,34 @@ python -m torch.distributed.launch --nproc_per_node=2 \
 #     configs/mask_rcnn_distill/mask_rcnn_distillation_lvis_base.py --launcher pytorch \
 #     --work-dir=${WORK_DIR} \
 #     --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=0.5 optimizer_config.grad_clip.max_norm=10 \
-#     #--resume-from=${WORK_DIR}/latest.pth
+#     --resume-from=${WORK_DIR}/latest.pth
 
+# base_filtered gt weight = 1, use gt only for distillation
+# WORK_DIR="/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_distillation_lvis_base_gt_only"
+# START_FROM="/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_distillation_lvis_base_12e_gt_only"
+# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+# python -m torch.distributed.launch --nproc_per_node=2 \
+#     /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+#     configs/mask_rcnn_distill/mask_rcnn_distillation_lvis_base_12e.py --launcher pytorch \
+#     --work-dir=${WORK_DIR} \
+#     --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=1 optimizer_config.grad_clip.max_norm=10 \
+#     model.train_cfg.rcnn.use_only_gt_pro_for_distill=True \
+#     --resume-from=${START_FROM}/latest.pth
+
+
+# base_filtered gt weight = 1, 12epoch, use range scale ???
+# WORK_DIR="/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_distillation_lvis_base_range_scale"
+# START_FROM="/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_distillation_lvis_base_12e_range_scale"
+# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+# python -m torch.distributed.launch --nproc_per_node=2 \
+#     /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+#     configs/mask_rcnn_distill/mask_rcnn_distillation_lvis_base_range_scale.py --launcher pytorch \
+#     --work-dir=${WORK_DIR} \
+#     --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=1 optimizer_config.grad_clip.max_norm=10 \
+#     --resume-from=${START_FROM}/epoch_8.pth
+
+
+### 4x setting experiments
 # base_filtered gt weight = 1, 48epoch
 # WORK_DIR="/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_distillation_lvis_base_48e"
 # PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
