dfc671a1e5e5c140ef38f71f35d1b62ff1b9e5e8
dragonlzm <dragonlzm123@gmail.com>
2022-10-04 11:06:23 -0700

    update for shell script

--- tools/temp3.sh
+++ tools/temp3.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 #SBATCH --partition=gpu 
 #SBATCH --gres=gpu:v100:2
 #SBATCH --ntasks=1
@@ -7,45 +6,16 @@
 #SBATCH --mem=80GB
 #SBATCH --time=48:00:00
 #SBATCH --account=nevatia_174
-
-
 module purge
 module load gcc/8.3.0
 #module load cuda/10.1.243
 #./program
-
 cd /project/nevatia_174/zhuoming/code/new_rpn/mmdetection
 #rm -rf ./data
 #ln -sf /project/nevatia_174/zhuoming/detection ./data
 
-### 2x4 experiment
-# 200 clip proposal filpping(merge3, 2x regression loss, base filtered proposal, per bbox weight)
-ADDITIONAL_CONFIG="model.backbone.merge_step=['merge3'] model.rpn_head.loss_bbox.loss_weight=2.0 model.roi_head.bbox_head.loss_bbox.loss_weight=2.0"
-WORK_DIR="/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_distillation_with_vit_lvis_base_12e_2x4"
-PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=2 \
-    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
-    configs/mask_rcnn_distill/mask_rcnn_distillation_with_vit_lvis_base_12e.py --launcher pytorch \
-    --work-dir=${WORK_DIR} \
-    --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=1 optimizer_config.grad_clip.max_norm=10 \
-    ${ADDITIONAL_CONFIG} \
-    data.samples_per_gpu=4 optimizer.lr=0.01 \
-    --resume-from=${WORK_DIR}/latest.pth
 
-
-
-# test the model
-#CHECKPOINT_NAME="epoch_12.pth"
-#CHECKPOINT_NAME="epoch_24.pth"
-CHECKPOINT_NAME="latest.pth"
-
-bash tools/dist_test.sh configs/mask_rcnn_distill/mask_rcnn_distillation_with_vit_lvis_bn.py \
-${WORK_DIR}/${CHECKPOINT_NAME} 2 --eval bbox segm \
---eval-options jsonfile_prefix=${WORK_DIR}/base_and_novel \
-${ADDITIONAL_CONFIG}
-
-
-
+#### part1
 ### 2x4 experiment
 # 200 clip proposal filpping(merge3, 2x regression loss, base filtered proposal, per bbox weight, 3x schedule, ln open, mlp)
 ADDITIONAL_CONFIG="model.backbone.merge_step=['merge3'] model.rpn_head.loss_bbox.loss_weight=2.0 model.roi_head.bbox_head.loss_bbox.loss_weight=2.0 model.vit_backbone_cfg.open_ln=True"
@@ -90,3 +60,46 @@ ${WORK_DIR}/${CHECKPOINT_NAME} 2 --eval bbox segm \
 --eval-options jsonfile_prefix=${WORK_DIR}/base_and_novel \
 --cfg-options data.test.eval_filter_empty_gt=False data.test.ann_file=data/coco/annotations/instances_val2017_65cates.json \
 ${ADDITIONAL_CONFIG}
+
+
+#### part2
+### v2 36e range scale, 2x reg
+ADDITIONAL_CONFIG="model.backbone.merge_step=['merge3'] model.rpn_head.loss_bbox.loss_weight=2.0 model.roi_head.bbox_head.loss_bbox.loss_weight=2.0"
+WORK_DIR="/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_distillation_with_vit_lvis_base_range_scale_36e"
+PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=2 \
+    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+    configs/mask_rcnn_distill/mask_rcnn_distillation_with_vit_lvis_base_range_scale_36e.py --launcher pytorch \
+    --work-dir=${WORK_DIR} \
+    --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=1 optimizer_config.grad_clip.max_norm=10 \
+    ${ADDITIONAL_CONFIG} \
+    --resume-from=${WORK_DIR}/latest.pth
+    #--resume-from=/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_distillation_with_vit_lvis_base_range_scale/epoch_16.pth
+
+# test the model
+CHECKPOINT_NAME="latest.pth"
+
+bash tools/dist_test.sh configs/mask_rcnn_distill/mask_rcnn_distillation_with_vit_lvis_bn.py \
+${WORK_DIR}/${CHECKPOINT_NAME} 2 --eval bbox segm \
+--eval-options jsonfile_prefix=${WORK_DIR}/base_and_novel \
+--cfg-options ${ADDITIONAL_CONFIG}
+
+
+#### part3
+# base_filtered gt weight = 1, 12epoch, use range scale
+WORK_DIR="/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_distillation_lvis_base_regw3"
+PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=2 \
+    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+    configs/mask_rcnn_distill/mask_rcnn_distillation_lvis_base.py --launcher pytorch \
+    --work-dir=${WORK_DIR} \
+    --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=1 optimizer_config.grad_clip.max_norm=10 \
+    model.rpn_head.loss_bbox.loss_weight=3.0 model.roi_head.bbox_head.loss_bbox.loss_weight=3.0 \
+    --resume-from=${WORK_DIR}/latest.pth
+# test the model
+#CHECKPOINT_NAME="epoch_12.pth"
+#CHECKPOINT_NAME="epoch_24.pth"
+CHECKPOINT_NAME="latest.pth"
+bash tools/dist_test.sh configs/mask_rcnn_distill/mask_rcnn_distillation_lvis_bn.py \
+${WORK_DIR}/${CHECKPOINT_NAME} 2 --eval bbox segm \
+--eval-options jsonfile_prefix=${WORK_DIR}/base_and_novel
