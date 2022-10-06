56f9bd15beef2c13ec310187c464e043ea95c5fd
dragonlzm <dragonlzm123@gmail.com>
2022-10-06 00:12:18 -0700

    Update faster_rcnn_r50_fpn_1x_coco_with_transformer.py

--- configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco_with_transformer.py
+++ configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco_with_transformer.py
@@ -18,6 +18,14 @@ model = dict(
     backbone=dict(
         init_cfg=dict(type='Pretrained', 
             checkpoint='data/pretrain/resnet50-0676ba61.pth')),
+    neck=dict(
+        type='FPN',
+        in_channels=[256, 512, 1024, 2048],
+        out_channels=256,
+        start_level=1,
+        add_extra_convs='on_output',  # use P5
+        num_outs=5,
+        relu_before_extra_convs=True),
     rpn_head=dict(
         type='TSPRPNHead',
         in_channels=256,
