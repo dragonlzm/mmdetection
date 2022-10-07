d6a62009fae878679a64835d4f634e1903862ae8
dragonlzm <dragonlzm123@gmail.com>
2022-10-07 13:24:07 -0700

    update for seesawloss 2x

--- mmdet/datasets/coco.py
+++ mmdet/datasets/coco.py
@@ -483,7 +483,7 @@ class CocoDataset(CustomDataset):
                 continue
             bboxes = []
             for ann in ann_info:
-                if ann.get('ignore', False) or ann['iscrowd']:
+                if ann.get('ignore', False) or ann.get('iscrowd', False):
                     continue
                 x1, y1, w, h = ann['bbox']
                 bboxes.append([x1, y1, x1 + w, y1 + h])
--- tools/mask_rcnn_distillation_lvis_seesaw_loss.sh
+++ tools/mask_rcnn_distillation_lvis_seesaw_loss.sh
@@ -19,13 +19,25 @@ cd /project/nevatia_174/zhuoming/code/new_rpn/mmdetection
 #ln -sf /project/nevatia_174/zhuoming/detection ./data
 
 # train lvis v1 range scale 1x
+# ADDITIONAL_CONFIG="model.rpn_head.loss_bbox.loss_weight=2.0 model.roi_head.bbox_head.loss_bbox.loss_weight=2.0"
+# WORK_DIR="/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_distillation_lvis_base_seesawloss"
+# bash tools/new_dist_train.sh configs/mask_rcnn_distill/mask_rcnn_distillation_lvis_base_seesawloss.py 2 \
+# ${WORK_DIR} ./data \
+# --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=0.5 optimizer_config.grad_clip.max_norm=10 \
+# ${ADDITIONAL_CONFIG} \
+# #--resume-from=${WORK_DIR}/latest.pth
+
+# train lvis v1 range scale 2x
+START_FROM="/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_distillation_lvis_base_seesawloss"
 ADDITIONAL_CONFIG="model.rpn_head.loss_bbox.loss_weight=2.0 model.roi_head.bbox_head.loss_bbox.loss_weight=2.0"
-WORK_DIR="/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_distillation_lvis_base_seesawloss"
-bash tools/new_dist_train.sh configs/mask_rcnn_distill/mask_rcnn_distillation_lvis_base_seesawloss.py 2 \
+WORK_DIR="/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_distillation_lvis_base_seesawloss_24e"
+bash tools/new_dist_train.sh configs/mask_rcnn_distill/mask_rcnn_distillation_lvis_base_seesawloss_24e.py 2 \
 ${WORK_DIR} ./data \
 --cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=0.5 optimizer_config.grad_clip.max_norm=10 \
 ${ADDITIONAL_CONFIG} \
+--resume-from=${START_FROM}/epoch_8.pth
 #--resume-from=${WORK_DIR}/latest.pth
+
 
 
 bash tools/dist_test.sh configs/mask_rcnn_distill/mask_rcnn_distillation_lvis_bn.py \
