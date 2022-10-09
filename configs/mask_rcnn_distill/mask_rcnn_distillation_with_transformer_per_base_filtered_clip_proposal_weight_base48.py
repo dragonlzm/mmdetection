51be7be32aa3abdba0fe3d8b82478692a8ffdcaf
dragonlzm <dragonlzm123@gmail.com>
2022-10-09 08:32:06 -0700

    update for config and shell script

--- configs/mask_rcnn_distill/mask_rcnn_distillation_with_transformer_per_base_filtered_clip_proposal_weight_base48.py
+++ configs/mask_rcnn_distill/mask_rcnn_distillation_with_transformer_per_base_filtered_clip_proposal_weight_base48.py
@@ -2,19 +2,21 @@ _base_ = './mask_rcnn_distillation_per_base_filtered_clip_proposal_weight.py'
 
 # regression with embedding, base filtered proposal, per distillation bbox weight
 # become default setting in here
-optimizer = dict(type='HybridOptimizer', lr=0.005, momentum=0.9, weight_decay=0.0001,
-                 constructor='HybridOptimizerConstructor', tranformer_multiplier=0.005)
-# learning policy
-lr_config = dict(
-    policy='step',
-    warmup='linear',
-    warmup_iters=500,
-    warmup_ratio=0.001,
-    step=[8, 11])
+optimizer = dict(lr=0.005)
+
+# optimizer = dict(type='HybridOptimizer', lr=0.005, momentum=0.9, weight_decay=0.0001,
+#                  constructor='HybridOptimizerConstructor', tranformer_multiplier=0.005)
+
+# optimizer_config = dict(_delete_=True, 
+#                         #type='ParamWiseOptimizerHook', 
+#                         grad_clip=dict(max_norm=10, norm_type=2))
 
 optimizer_config = dict(_delete_=True, 
-                        #type='ParamWiseOptimizerHook', 
-                        grad_clip=dict(max_norm=10, norm_type=2))
+                        type='ParamWiseGradientCumulativeOptimizerHook', 
+                        cumulative_iters=1,
+                        grad_clip=dict(encoder=dict(max_norm=0.01, norm_type=2), 
+                                       other=dict(max_norm=10, norm_type=2)))
+
 
 # model settings
 model = dict(
@@ -22,9 +24,10 @@ model = dict(
         type='StandardRoIHeadDistillWithTransformer',
         bbox_head=dict(
             type='TransformerEmbeddingBBoxHead',
-            num_shared_convs=4,
+            num_shared_convs=0,
+            num_shared_fcs=1,
+            with_avg_pool=False,
             reg_class_agnostic=True,
-            in_channels=256,
             reg_with_cls_embedding=False,
             fc_out_channels=512,
             encoder=dict(
@@ -47,4 +50,14 @@ model = dict(
                             ffn_drop=0.1,
                             act_cfg=dict(type='ReLU', inplace=True),
                     ),
-                    operation_order=('self_attn', 'norm', 'ffn', 'norm'))))))
+                    operation_order=('self_attn', 'norm', 'ffn', 'norm'))))),
+    train_cfg=dict(
+        rcnn=dict(
+            sampler=dict(
+                type='DoubleRandomSampler',
+                num=512,
+                pos_fraction=0.25,
+                neg_pos_ub=-1,
+                add_gt_as_proposals=True),
+            pos_weight=-1,
+            debug=False)))
