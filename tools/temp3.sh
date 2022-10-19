f65975ea7604f9976192c124742741c5b3d285d2
dragonlzm <dragonlzm123@gmail.com>
2022-10-19 09:01:52 -0700

    Update temp3.sh

--- tools/temp3.sh
+++ tools/temp3.sh
@@ -50,3 +50,19 @@ CHECKPOINT_NAME="latest.pth"
 bash tools/dist_test.sh configs/mask_rcnn_distill/mask_rcnn_distillation_with_vit_lvis_bn_seesawloss.py \
 ${WORK_DIR}/${CHECKPOINT_NAME} 2 --eval bbox segm \
 --eval-options jsonfile_prefix=${WORK_DIR}/base_and_novel
+
+
+# train lvis v2 3xregw 0.5distw, range scale, 12 epoch
+ADDITIONAL_CONFIG="model.rpn_head.loss_bbox.loss_weight=3.0 model.roi_head.bbox_head.loss_bbox.loss_weight=3.0"
+WORK_DIR="/project/nevatia_174/zhuoming/detection/grad_clip_check/mask_rcnn_distillation_lvis_base_seesawloss_24e_3xreg_05xdistw_rangescale"
+bash tools/new_dist_train.sh configs/mask_rcnn_distill/mask_rcnn_distillation_with_vit_lvis_base_range_scale_seesawloss_12e.py 2 \
+${WORK_DIR} ./data \
+--cfg-options model.roi_head.bbox_head.temperature=100 model.train_cfg.rcnn.distill_loss_factor=0.5 optimizer_config.grad_clip.max_norm=10 \
+${ADDITIONAL_CONFIG} \
+--resume-from=${WORK_DIR}/latest.pth
+
+
+CHECKPOINT_NAME="latest.pth"
+bash tools/dist_test.sh configs/mask_rcnn_distill/mask_rcnn_distillation_with_vit_lvis_bn_seesawloss.py \
+${WORK_DIR}/${CHECKPOINT_NAME} 2 --eval bbox segm \
+--eval-options jsonfile_prefix=${WORK_DIR}/base_and_novel
