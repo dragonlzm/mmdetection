d6235cfcfb8b2e622d6fb7c8b585d6bff5052b35
dragonlzm <dragonlzm123@gmail.com>
2022-10-20 13:52:15 -0700

    Create mask_rcnn_distillation_per_base_filtered_clip_proposal_weight_zero_bg_vec.py

---
+++ configs/mask_rcnn_distill/mask_rcnn_distillation_per_base_filtered_clip_proposal_weight_zero_bg_vec.py
@@ -0,0 +1,7 @@
+_base_ = './mask_rcnn_distillation_per_base_filtered_clip_proposal_weight.py'
+
+# model settings
+model = dict(
+    roi_head=dict(
+        bbox_head=dict(
+            use_zero_bg_vector=True)))
