10ccf87eaaae973c9dcb1a700cfe41b6beff4b6b
dragonlzm <dragonlzm123@gmail.com>
2022-10-29 11:29:47 -0700

    Update mask_rcnn_distillation_voc_split1_base_r101_fewshot_setting_bs16.py

--- configs/mask_rcnn_distill/mask_rcnn_distillation_voc_split1_base_r101_fewshot_setting_bs16.py
+++ configs/mask_rcnn_distill/mask_rcnn_distillation_voc_split1_base_r101_fewshot_setting_bs16.py
@@ -1,9 +1,9 @@
 _base_ = './mask_rcnn_distillation_voc_split1_base_r101_fewshot_setting.py'
 
-#  for 4*4
-data = dict(
-    samples_per_gpu=4,
-    workers_per_gpu=2)
 
 # optimizer for 4*4
 optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(_delete_=True, 
+                        type='GradientCumulativeOptimizerHook', 
+                        cumulative_iters=2,
+                        grad_clip=dict(max_norm=10, norm_type=2))
