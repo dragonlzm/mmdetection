c0e1486f6007973b228ac90ff13b6fcf2b3cfbe4
dragonlzm <dragonlzm123@gmail.com>
2022-10-29 11:50:49 -0700

    Update mask_rcnn_distillation_voc_split1_base_r101_fewshot_setting_bs16.py

--- configs/mask_rcnn_distill/mask_rcnn_distillation_voc_split1_base_r101_fewshot_setting_bs16.py
+++ configs/mask_rcnn_distill/mask_rcnn_distillation_voc_split1_base_r101_fewshot_setting_bs16.py
@@ -1,9 +1,9 @@
 _base_ = './mask_rcnn_distillation_voc_split1_base_r101_fewshot_setting.py'
 
 
-# optimizer for 4*4
+# optimizer for simulating the 4*4 by accumulate the 2*2 4 times
 optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(_delete_=True, 
                         type='GradientCumulativeOptimizerHook', 
-                        cumulative_iters=2,
+                        cumulative_iters=4,
                         grad_clip=dict(max_norm=10, norm_type=2))
