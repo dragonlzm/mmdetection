31300b404060bcf3c38c69f0c8aab81df6670f8d
dragonlzm <dragonlzm123@gmail.com>
2022-11-01 19:57:27 -0700

    update for shell script

--- tools/cluster_script/clip_proposal_generation/clip_proposal_generation_lvis_lvis1203_13.sh
+++ tools/cluster_script/clip_proposal_generation/clip_proposal_generation_lvis_lvis1203_13.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:v100:2
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=80GB
+#SBATCH --mem=30GB
 #SBATCH --time=48:00:00
 #SBATCH --account=nevatia_174
 
@@ -170,12 +170,12 @@ python -m torch.distributed.launch --nproc_per_node=2 \
     data.test.ann_file=data/lvis_v1/annotations/lvis_v1_train_96000_104000.json 
 
 # the remain
-PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=2 \
-    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/test.py \
-    ${CONFIG_FILE} \
-    ${CHECKPOINT} \
-    --launcher pytorch --eval=proposal_fast --options jsonfile_prefix=${JSONFILE_PREFIX}_1 \
-    --cfg-options model.anchor_generator.strides=[32] model.anchor_generator.scales=[1,2,4,8,16] model.test_cfg.nms_on_all_anchors=True \
-    model.test_cfg.nms_threshold=0.7 model.test_cfg.min_entropy=True model.test_cfg.bbox_save_path_root=${BBOX_SAVE_PATH_ROOT} \
-    data.test.ann_file=data/lvis_v1/annotations/lvis_v1_train.json 
+# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+# python -m torch.distributed.launch --nproc_per_node=2 \
+#     /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/test.py \
+#     ${CONFIG_FILE} \
+#     ${CHECKPOINT} \
+#     --launcher pytorch --eval=proposal_fast --options jsonfile_prefix=${JSONFILE_PREFIX}_1 \
+#     --cfg-options model.anchor_generator.strides=[32] model.anchor_generator.scales=[1,2,4,8,16] model.test_cfg.nms_on_all_anchors=True \
+#     model.test_cfg.nms_threshold=0.7 model.test_cfg.min_entropy=True model.test_cfg.bbox_save_path_root=${BBOX_SAVE_PATH_ROOT} \
+#     data.test.ann_file=data/lvis_v1/annotations/lvis_v1_train.json 
