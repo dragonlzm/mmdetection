41ae1f4bc66e3746ecd84987ade494e8f978d004
dragonlzm <dragonlzm123@gmail.com>
2022-11-15 19:08:26 -0800

    update for lvis feature

--- configs/mask_rcnn_distill/mask_rcnn_distillation_lvis_base_12e_range_scale.py
+++ configs/mask_rcnn_distill/mask_rcnn_distillation_lvis_base_12e_range_scale.py
@@ -1,6 +1,7 @@
 _base_ = './mask_rcnn_distillation_lvis_base_12e.py'
 
 # change the data pipeline
+# try different feature.
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
@@ -11,7 +12,7 @@ train_pipeline = [
         img_scale=[(1333, 640), (1333, 800)],
         multiscale_mode='range',
         keep_ratio=True),    
-    dict(type='LoadCLIPFeat', file_path_prefix='data/lvis_v1/clip_proposal_feat/lvis_base_finetuned',
+    dict(type='LoadCLIPFeat', file_path_prefix='data/lvis_v1/clip_proposal_feat/lvis_base_finetuned_vision_and_text',
          num_of_rand_bbox=200, select_fixed_subset=200, load_rand_bbox_weight=True),    
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
