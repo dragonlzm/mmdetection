bc20cbc3396329505d24b4fedb13152788f4dfab
dragonlzm <dragonlzm123@gmail.com>
2022-12-07 22:09:52 -0800

    Create retinanet_r50_fpn_1x_coco_base48.py

---
+++ configs/retinanet/retinanet_r50_fpn_1x_coco_base48.py
@@ -0,0 +1,20 @@
+_base_ = './retinanet_r50_fpn_1x_coco.py'
+
+classes = ('person', 'bicycle', 'car', 'motorcycle', 'train', 
+            'truck', 'boat', 'bench', 'bird', 'horse', 'sheep', 
+            'bear', 'zebra', 'giraffe', 'backpack', 'handbag', 
+            'suitcase', 'frisbee', 'skis', 'kite', 'surfboard', 
+            'bottle', 'fork', 'spoon', 'bowl', 'banana', 'apple', 
+            'sandwich', 'orange', 'broccoli', 'carrot', 'pizza', 
+            'donut', 'chair', 'bed', 'toilet', 'tv', 'laptop', 
+            'mouse', 'remote', 'microwave', 'oven', 'toaster', 
+            'refrigerator', 'book', 'clock', 'vase', 'toothbrush')
+
+data = dict(
+    train=dict(classes=classes),
+    val=dict(classes=classes),
+    test=dict(classes=classes))
+
+# model settings
+model = dict(
+    bbox_head=dict(num_classes=48))
