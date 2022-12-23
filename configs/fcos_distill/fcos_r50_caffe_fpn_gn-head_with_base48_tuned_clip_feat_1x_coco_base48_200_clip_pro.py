d4e264c3c1e751f2fed9cd45fff72af79c0b3023
dragonlzm <dragonlzm123@gmail.com>
2022-12-23 11:47:00 -0800

    update for config

--- configs/fcos_distill/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_base48_200_clip_pro.py
+++ configs/fcos_distill/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_base48_200_clip_pro.py
@@ -6,9 +6,9 @@ train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
     dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
-    dict(type='RandomFlip', flip_ratio=0.0),
-    dict(type='LoadCLIPFeat', file_path_prefix='data/coco/feat/base48_finetuned_500',
+    dict(type='LoadCLIPFeat', file_path_prefix='data/coco/clip_proposal_feat/base48_finetuned_base_filtered',
          num_of_rand_bbox=200, select_fixed_subset=200),
+    dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
@@ -19,3 +19,12 @@ train_pipeline = [
 data = dict(
     train=dict(
         pipeline=train_pipeline))
+
+# optimizer
+optimizer_config = dict(
+    _delete_=True, grad_clip=dict(max_norm=10, norm_type=2))
+
+model = dict(
+    backbone=dict(
+        init_cfg=dict(type='Pretrained', 
+            checkpoint='open-mmlab://detectron/resnet50_caffe')))
--- configs/retinanet_distill/retinanet_distill_r50_fpn_1x_coco.py
+++ configs/retinanet_distill/retinanet_distill_r50_fpn_1x_coco.py
@@ -9,7 +9,7 @@ img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+    dict(type='LoadAnnotations', with_bbox=True),
     dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
     dict(type='LoadCLIPFeat', file_path_prefix='data/coco/clip_proposal_feat/base48_finetuned_base_filtered',
          num_of_rand_bbox=200, select_fixed_subset=200, load_rand_bbox_weight=True),    
