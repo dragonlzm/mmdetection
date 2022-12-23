a32e59d58d2d2fc035514893ee9587963296eb21
dragonlzm <dragonlzm123@gmail.com>
2022-12-23 12:09:23 -0800

    Update fcos_r50_caffe_fpn_gn-head_with_clip_feat.sh

--- tools/fcos_r50_caffe_fpn_gn-head_with_clip_feat.sh
+++ tools/fcos_r50_caffe_fpn_gn-head_with_clip_feat.sh
@@ -44,14 +44,14 @@ cd /project/nevatia_174/zhuoming/code/new_rpn/mmdetection
 #    #--resume-from=/project/nevatia_174/zhuoming/detection/fcos/fcos_r50_caffe_fpn_gn-head_1x_coco_wo_centerness_base48/latest.pth
 
 # 48 cates 200 clip proposal
-WORK_DIR="/project/nevatia_174/zhuoming/detection/fcos/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_base48_200_clip_pro_w01"
-PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=2 \
-   /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
-   configs/fcos_distill/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_base48_200_clip_pro.py --launcher pytorch \
-   --work-dir=${WORK_DIR} \
-   --cfg-options model.train_cfg.distill_loss_factor=0.1 \
-   --resume-from=${WORK_DIR}/latest.pth
+# WORK_DIR="/project/nevatia_174/zhuoming/detection/fcos/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_base48_200_clip_pro_w01"
+# PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+# python -m torch.distributed.launch --nproc_per_node=2 \
+#    /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+#    configs/fcos_distill/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_base48_200_clip_pro.py --launcher pytorch \
+#    --work-dir=${WORK_DIR} \
+#    --cfg-options model.train_cfg.distill_loss_factor=0.1 \
+#    --resume-from=${WORK_DIR}/latest.pth
 
 # WORK_DIR="/project/nevatia_174/zhuoming/detection/fcos/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_base48_200_clip_pro_w002"
 # PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
@@ -71,21 +71,21 @@ python -m torch.distributed.launch --nproc_per_node=2 \
 #    --cfg-options model.train_cfg.distill_loss_factor=0.5 \
 #    --resume-from=${WORK_DIR}/latest.pth
 
+
+####### update the hyper parameters
+WORK_DIR="/project/nevatia_174/zhuoming/detection/one_stage/fcos_distill_new"
+PYTHONPATH="/project/nevatia_174/zhuoming/code/new_rpn/mmdetection":$PYTHONPATH \
+python -m torch.distributed.launch --nproc_per_node=2 \
+   /project/nevatia_174/zhuoming/code/new_rpn/mmdetection/tools/train.py \
+   configs/fcos_distill/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_base48_200_clip_pro.py --launcher pytorch \
+   --work-dir=${WORK_DIR} \
+   --cfg-options optimizer.lr=0.0025  \
+#   --resume-from=${WORK_DIR}/latest.pth
+
+
 # test the model 
-bash tools/dist_test.sh configs/fcos_distill/fcos_r50_caffe_fpn_gn-head_with_clip_feat_1x_coco_base48.py \
-${WORK_DIR}/epoch_12.pth 2 --eval bbox \
---eval-options jsonfile_prefix=${WORK_DIR}/base \
+bash tools/dist_test.sh configs/fcos_distill/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_bn65.py \
+${WORK_DIR}/latest.pth 2 --eval bbox \
+--eval-options jsonfile_prefix=${WORK_DIR}/base_and_novel \
 --cfg-options data.test.eval_filter_empty_gt=False data.test.ann_file=data/coco/annotations/instances_val2017_65cates.json \
-model.test_cfg.score_thr=0.0 model.test_cfg.max_per_img=300
-
-bash tools/dist_test.sh configs/fcos_distill/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_base48.py \
-${WORK_DIR}/epoch_12.pth 2 --eval bbox \
---eval-options jsonfile_prefix=${WORK_DIR}/base_results \
---cfg-options data.test.eval_filter_empty_gt=False data.test.ann_file=data/coco/annotations/instances_val2017_65cates.json \
-model.test_cfg.score_thr=0.0 model.test_cfg.max_per_img=300
-
-bash tools/dist_test.sh configs/fcos_distill/fcos_r50_caffe_fpn_gn-head_with_base48_tuned_clip_feat_1x_coco_novel17.py \
-${WORK_DIR}/epoch_12.pth 2 --eval bbox \
---eval-options jsonfile_prefix=${WORK_DIR}/novel_results \
---cfg-options data.test.eval_filter_empty_gt=False data.test.ann_file=data/coco/annotations/instances_val2017_65cates.json \
-model.test_cfg.score_thr=0.0 model.test_cfg.max_per_img=300
+model.roi_head.bbox_head.reg_with_cls_embedding=True data.test.eval_on_splits='zeroshot'
