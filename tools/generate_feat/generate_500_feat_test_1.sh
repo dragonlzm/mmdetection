4c496e064a1aa7d81780e8053db19fa7d6b12927
dragonlzm <dragonlzm123@gmail.com>
2022-12-29 14:59:20 -0800

    update for feature extraction

--- tools/generate_feat/generate_500_feat_test_1.sh
+++ tools/generate_feat/generate_500_feat_test_1.sh
@@ -26,68 +26,75 @@ cd /project/nevatia_174/zhuoming/code/new_rpn/mmdetection
 # rm -rf ./data
 # ln -sf /project/nevatia_174/zhuoming/detection ./data
 
-bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_full_coco.py \
-data/exp_res/cls_finetuner_clip_base48_all_train/epoch_12.pth 2 --eval=gt_acc \
+
+CONFIG_FILE="configs/cls_finetuner/cls_finetuner_clip_full_coco.py"
+CHECKPOINT="data/exp_res/cls_finetuner_clip_base48_all_train/epoch_12.pth"
+FEAT_SAVE_PATH_ROOT="data/coco/clip_proposal_feat/base48_finetuned_channel_corr"
+#BBOX_SAVE_PATH_ROOT="data/coco/clip_proposal/32_32_512"
+BBOX_SAVE_PATH_ROOT="data/coco/clip_proposal/32_32_512_channel_corr"
+
+bash tools/dist_test.sh ${CONFIG_FILE} \
+${CHECKPOINT} 2 --eval=gt_acc \
 --eval-options jsonfile_prefix=data/coco/clip_proposal_feat/base48_finetuned/add_conf \
 --cfg-options data.test.ann_file=data/coco/annotations/instances_train2017_112000_120000.json data.test.img_prefix=data/coco/train2017/ \
-   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=data/coco/clip_proposal_feat/base48_finetuned_base_filtered_new \
-   model.test_cfg.use_pregenerated_proposal=data/coco/clip_proposal/32_32_512 \
+   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=${FEAT_SAVE_PATH_ROOT} \
+   model.test_cfg.use_pregenerated_proposal=${BBOX_SAVE_PATH_ROOT} \
    model.test_cfg.num_of_rand_bboxes=500 model.test_cfg.save_cates_and_conf=True
 
-bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_full_coco.py \
-data/exp_res/cls_finetuner_clip_base48_all_train/epoch_12.pth 2 --eval=gt_acc \
+bash tools/dist_test.sh ${CONFIG_FILE} \
+${CHECKPOINT} 2 --eval=gt_acc \
 --eval-options jsonfile_prefix=data/coco/clip_proposal_feat/base48_finetuned/add_conf \
 --cfg-options data.test.ann_file=data/coco/annotations/instances_train2017_104000_112000.json data.test.img_prefix=data/coco/train2017/ \
-   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=data/coco/clip_proposal_feat/base48_finetuned_base_filtered_new \
-   model.test_cfg.use_pregenerated_proposal=data/coco/clip_proposal/32_32_512 \
+   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=${FEAT_SAVE_PATH_ROOT} \
+   model.test_cfg.use_pregenerated_proposal=${BBOX_SAVE_PATH_ROOT} \
    model.test_cfg.num_of_rand_bboxes=500 model.test_cfg.save_cates_and_conf=True
 
-bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_full_coco.py \
-data/exp_res/cls_finetuner_clip_base48_all_train/epoch_12.pth 2 --eval=gt_acc \
+bash tools/dist_test.sh ${CONFIG_FILE} \
+${CHECKPOINT} 2 --eval=gt_acc \
 --eval-options jsonfile_prefix=data/coco/clip_proposal_feat/base48_finetuned/add_conf \
 --cfg-options data.test.ann_file=data/coco/annotations/instances_train2017_96000_104000.json data.test.img_prefix=data/coco/train2017/ \
-   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=data/coco/clip_proposal_feat/base48_finetuned_base_filtered_new \
-   model.test_cfg.use_pregenerated_proposal=data/coco/clip_proposal/32_32_512 \
+   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=${FEAT_SAVE_PATH_ROOT} \
+   model.test_cfg.use_pregenerated_proposal=${BBOX_SAVE_PATH_ROOT} \
    model.test_cfg.num_of_rand_bboxes=500 model.test_cfg.save_cates_and_conf=True
 
-bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_full_coco.py \
-data/exp_res/cls_finetuner_clip_base48_all_train/epoch_12.pth 2 --eval=gt_acc \
+bash tools/dist_test.sh ${CONFIG_FILE} \
+${CHECKPOINT} 2 --eval=gt_acc \
 --eval-options jsonfile_prefix=data/coco/clip_proposal_feat/base48_finetuned/add_conf \
 --cfg-options data.test.ann_file=data/coco/annotations/instances_train2017_88000_96000.json data.test.img_prefix=data/coco/train2017/ \
-   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=data/coco/clip_proposal_feat/base48_finetuned_base_filtered_new \
-   model.test_cfg.use_pregenerated_proposal=data/coco/clip_proposal/32_32_512 \
+   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=${FEAT_SAVE_PATH_ROOT} \
+   model.test_cfg.use_pregenerated_proposal=${BBOX_SAVE_PATH_ROOT} \
    model.test_cfg.num_of_rand_bboxes=500 model.test_cfg.save_cates_and_conf=True
 
-bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_full_coco.py \
-data/exp_res/cls_finetuner_clip_base48_all_train/epoch_12.pth 2 --eval=gt_acc \
+bash tools/dist_test.sh ${CONFIG_FILE} \
+${CHECKPOINT} 2 --eval=gt_acc \
 --eval-options jsonfile_prefix=data/coco/clip_proposal_feat/base48_finetuned/add_conf \
 --cfg-options data.test.ann_file=data/coco/annotations/instances_train2017_80000_88000.json data.test.img_prefix=data/coco/train2017/ \
-   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=data/coco/clip_proposal_feat/base48_finetuned_base_filtered_new \
-   model.test_cfg.use_pregenerated_proposal=data/coco/clip_proposal/32_32_512 \
+   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=${FEAT_SAVE_PATH_ROOT} \
+   model.test_cfg.use_pregenerated_proposal=${BBOX_SAVE_PATH_ROOT} \
    model.test_cfg.num_of_rand_bboxes=500 model.test_cfg.save_cates_and_conf=True
 
-bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_full_coco.py \
-data/exp_res/cls_finetuner_clip_base48_all_train/epoch_12.pth 2 --eval=gt_acc \
+bash tools/dist_test.sh ${CONFIG_FILE} \
+${CHECKPOINT} 2 --eval=gt_acc \
 --eval-options jsonfile_prefix=data/coco/clip_proposal_feat/base48_finetuned/add_conf \
 --cfg-options data.test.ann_file=data/coco/annotations/instances_train2017_72000_80000.json data.test.img_prefix=data/coco/train2017/ \
-   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=data/coco/clip_proposal_feat/base48_finetuned_base_filtered_new \
-   model.test_cfg.use_pregenerated_proposal=data/coco/clip_proposal/32_32_512 \
+   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=${FEAT_SAVE_PATH_ROOT} \
+   model.test_cfg.use_pregenerated_proposal=${BBOX_SAVE_PATH_ROOT} \
    model.test_cfg.num_of_rand_bboxes=500 model.test_cfg.save_cates_and_conf=True
 
-bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_full_coco.py \
-data/exp_res/cls_finetuner_clip_base48_all_train/epoch_12.pth 2 --eval=gt_acc \
+bash tools/dist_test.sh ${CONFIG_FILE} \
+${CHECKPOINT} 2 --eval=gt_acc \
 --eval-options jsonfile_prefix=data/coco/clip_proposal_feat/base48_finetuned/add_conf \
 --cfg-options data.test.ann_file=data/coco/annotations/instances_train2017_64000_72000.json data.test.img_prefix=data/coco/train2017/ \
-   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=data/coco/clip_proposal_feat/base48_finetuned_base_filtered_new \
-   model.test_cfg.use_pregenerated_proposal=data/coco/clip_proposal/32_32_512 \
+   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=${FEAT_SAVE_PATH_ROOT} \
+   model.test_cfg.use_pregenerated_proposal=${BBOX_SAVE_PATH_ROOT} \
    model.test_cfg.num_of_rand_bboxes=500 model.test_cfg.save_cates_and_conf=True
 
-bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_full_coco.py \
-data/exp_res/cls_finetuner_clip_base48_all_train/epoch_12.pth 2 --eval=gt_acc \
+bash tools/dist_test.sh ${CONFIG_FILE} \
+${CHECKPOINT} 2 --eval=gt_acc \
 --eval-options jsonfile_prefix=data/coco/clip_proposal_feat/base48_finetuned/add_conf \
 --cfg-options data.test.ann_file=data/coco/annotations/instances_train2017.json data.test.img_prefix=data/coco/train2017/ \
-   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=data/coco/clip_proposal_feat/base48_finetuned_base_filtered_new \
-   model.test_cfg.use_pregenerated_proposal=data/coco/clip_proposal/32_32_512 \
+   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=${FEAT_SAVE_PATH_ROOT} \
+   model.test_cfg.use_pregenerated_proposal=${BBOX_SAVE_PATH_ROOT} \
    model.test_cfg.num_of_rand_bboxes=500 model.test_cfg.save_cates_and_conf=True
 
 # cd ..
--- tools/generate_feat/generate_500_feat_test_2.sh
+++ tools/generate_feat/generate_500_feat_test_2.sh
@@ -25,70 +25,72 @@ cd /project/nevatia_174/zhuoming/code/new_rpn/mmdetection
 # cd mmdetection
 # rm -rf ./data
 # ln -sf /project/nevatia_174/zhuoming/detection ./data
+CONFIG_FILE="configs/cls_finetuner/cls_finetuner_clip_full_coco.py"
+CHECKPOINT="data/exp_res/cls_finetuner_clip_base48_all_train/epoch_12.pth"
+FEAT_SAVE_PATH_ROOT="data/coco/clip_proposal_feat/base48_finetuned_channel_corr"
+#BBOX_SAVE_PATH_ROOT="data/coco/clip_proposal/32_32_512"
+BBOX_SAVE_PATH_ROOT="data/coco/clip_proposal/32_32_512_channel_corr"
 
-bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_full_coco.py \
-data/exp_res/cls_finetuner_clip_base48_all_train/epoch_12.pth 2 --eval=gt_acc \
+bash tools/dist_test.sh ${CONFIG_FILE} \
+${CHECKPOINT} 2 --eval=gt_acc \
 --eval-options jsonfile_prefix=data/coco/clip_proposal_feat/base48_finetuned/add_conf \
 --cfg-options data.test.ann_file=data/coco/annotations/instances_train2017_56000_64000.json data.test.img_prefix=data/coco/train2017/ \
-   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=data/coco/clip_proposal_feat/base48_finetuned_base_filtered_new \
-   model.test_cfg.use_pregenerated_proposal=data/coco/clip_proposal/32_32_512 \
+   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=${FEAT_SAVE_PATH_ROOT} \
+   model.test_cfg.use_pregenerated_proposal=${BBOX_SAVE_PATH_ROOT} \
    model.test_cfg.num_of_rand_bboxes=500 model.test_cfg.save_cates_and_conf=True
 
-bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_full_coco.py \
-data/exp_res/cls_finetuner_clip_base48_all_train/epoch_12.pth 2 --eval=gt_acc \
+bash tools/dist_test.sh ${CONFIG_FILE} \
+${CHECKPOINT} 2 --eval=gt_acc \
 --eval-options jsonfile_prefix=data/coco/clip_proposal_feat/base48_finetuned/add_conf \
 --cfg-options data.test.ann_file=data/coco/annotations/instances_train2017_48000_56000.json data.test.img_prefix=data/coco/train2017/ \
-   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=data/coco/clip_proposal_feat/base48_finetuned_base_filtered_new \
-   model.test_cfg.use_pregenerated_proposal=data/coco/clip_proposal/32_32_512 \
+   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=${FEAT_SAVE_PATH_ROOT} \
+   model.test_cfg.use_pregenerated_proposal=${BBOX_SAVE_PATH_ROOT} \
    model.test_cfg.num_of_rand_bboxes=500 model.test_cfg.save_cates_and_conf=True
 
-bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_full_coco.py \
-data/exp_res/cls_finetuner_clip_base48_all_train/epoch_12.pth 2 --eval=gt_acc \
+bash tools/dist_test.sh ${CONFIG_FILE} \
+${CHECKPOINT} 2 --eval=gt_acc \
 --eval-options jsonfile_prefix=data/coco/clip_proposal_feat/base48_finetuned/add_conf \
 --cfg-options data.test.ann_file=data/coco/annotations/instances_train2017_40000_48000.json data.test.img_prefix=data/coco/train2017/ \
-   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=data/coco/clip_proposal_feat/base48_finetuned_base_filtered_new \
-   model.test_cfg.use_pregenerated_proposal=data/coco/clip_proposal/32_32_512 \
+   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=${FEAT_SAVE_PATH_ROOT} \
+   model.test_cfg.use_pregenerated_proposal=${BBOX_SAVE_PATH_ROOT} \
    model.test_cfg.num_of_rand_bboxes=500 model.test_cfg.save_cates_and_conf=True
 
-bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_full_coco.py \
-data/exp_res/cls_finetuner_clip_base48_all_train/epoch_12.pth 2 --eval=gt_acc \
+bash tools/dist_test.sh ${CONFIG_FILE} \
+${CHECKPOINT} 2 --eval=gt_acc \
 --eval-options jsonfile_prefix=data/coco/clip_proposal_feat/base48_finetuned/add_conf \
 --cfg-options data.test.ann_file=data/coco/annotations/instances_train2017_32000_40000.json data.test.img_prefix=data/coco/train2017/ \
-   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=data/coco/clip_proposal_feat/base48_finetuned_base_filtered_new \
-   model.test_cfg.use_pregenerated_proposal=data/coco/clip_proposal/32_32_512 \
+   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=${FEAT_SAVE_PATH_ROOT} \
+   model.test_cfg.use_pregenerated_proposal=${BBOX_SAVE_PATH_ROOT} \
    model.test_cfg.num_of_rand_bboxes=500 model.test_cfg.save_cates_and_conf=True
 
-bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_full_coco.py \
-data/exp_res/cls_finetuner_clip_base48_all_train/epoch_12.pth 2 --eval=gt_acc \
+bash tools/dist_test.sh ${CONFIG_FILE} \
+${CHECKPOINT} 2 --eval=gt_acc \
 --eval-options jsonfile_prefix=data/coco/clip_proposal_feat/base48_finetuned/add_conf \
 --cfg-options data.test.ann_file=data/coco/annotations/instances_train2017_24000_32000.json data.test.img_prefix=data/coco/train2017/ \
-   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=data/coco/clip_proposal_feat/base48_finetuned_base_filtered_new \
-   model.test_cfg.use_pregenerated_proposal=data/coco/clip_proposal/32_32_512 \
+   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=${FEAT_SAVE_PATH_ROOT} \
+   model.test_cfg.use_pregenerated_proposal=${BBOX_SAVE_PATH_ROOT} \
    model.test_cfg.num_of_rand_bboxes=500 model.test_cfg.save_cates_and_conf=True
 
-bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_full_coco.py \
-data/exp_res/cls_finetuner_clip_base48_all_train/epoch_12.pth 2 --eval=gt_acc \
+bash tools/dist_test.sh ${CONFIG_FILE} \
+${CHECKPOINT} 2 --eval=gt_acc \
 --eval-options jsonfile_prefix=data/coco/clip_proposal_feat/base48_finetuned/add_conf \
 --cfg-options data.test.ann_file=data/coco/annotations/instances_train2017_16000_24000.json data.test.img_prefix=data/coco/train2017/ \
-   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=data/coco/clip_proposal_feat/base48_finetuned_base_filtered_new \
-   model.test_cfg.use_pregenerated_proposal=data/coco/clip_proposal/32_32_512 \
+   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=${FEAT_SAVE_PATH_ROOT} \
+   model.test_cfg.use_pregenerated_proposal=${BBOX_SAVE_PATH_ROOT} \
    model.test_cfg.num_of_rand_bboxes=500 model.test_cfg.save_cates_and_conf=True
 
-bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_full_coco.py \
-data/exp_res/cls_finetuner_clip_base48_all_train/epoch_12.pth 2 --eval=gt_acc \
+bash tools/dist_test.sh ${CONFIG_FILE} \
+${CHECKPOINT} 2 --eval=gt_acc \
 --eval-options jsonfile_prefix=data/coco/clip_proposal_feat/base48_finetuned/add_conf \
 --cfg-options data.test.ann_file=data/coco/annotations/instances_train2017_8000_16000.json data.test.img_prefix=data/coco/train2017/ \
-   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=data/coco/clip_proposal_feat/base48_finetuned_base_filtered_new \
-   model.test_cfg.use_pregenerated_proposal=data/coco/clip_proposal/32_32_512 \
+   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=${FEAT_SAVE_PATH_ROOT} \
+   model.test_cfg.use_pregenerated_proposal=${BBOX_SAVE_PATH_ROOT} \
    model.test_cfg.num_of_rand_bboxes=500 model.test_cfg.save_cates_and_conf=True
 
-bash tools/dist_test.sh configs/cls_finetuner/cls_finetuner_clip_full_coco.py \
-data/exp_res/cls_finetuner_clip_base48_all_train/epoch_12.pth 2 --eval=gt_acc \
+bash tools/dist_test.sh ${CONFIG_FILE} \
+${CHECKPOINT} 2 --eval=gt_acc \
 --eval-options jsonfile_prefix=data/coco/clip_proposal_feat/base48_finetuned/add_conf \
 --cfg-options data.test.ann_file=data/coco/annotations/instances_train2017_0_8000.json data.test.img_prefix=data/coco/train2017/ \
-   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=data/coco/clip_proposal_feat/base48_finetuned_base_filtered_new \
-   model.test_cfg.use_pregenerated_proposal=data/coco/clip_proposal/32_32_512 \
+   model.test_cfg.generate_bbox_feat=True model.test_cfg.feat_save_path=${FEAT_SAVE_PATH_ROOT} \
+   model.test_cfg.use_pregenerated_proposal=${BBOX_SAVE_PATH_ROOT} \
    model.test_cfg.num_of_rand_bboxes=500 model.test_cfg.save_cates_and_conf=True
-
-# cd ..
-# rm -rf mmdetection
