97400e075380deff8ebaf8797bf6890a003d0d1a
dragonlzm <dragonlzm123@gmail.com>
2023-01-04 23:23:41 -0800

    Update generate_dataset_subset_randomly.py

--- generate_dataset_subset_randomly.py
+++ generate_dataset_subset_randomly.py
@@ -1,19 +1,18 @@
 # this script aims to generate dataset subset by randomly select some annotations and images.
-
 import json
 import random
-from select import select
 
+##### generate dataset subset
 # fix the random seed
 random.seed(42)
 
-json_content = json.load(open('/data2/lwll/zhuoming/detection/coco/annotations/instances_train2017.json'))
+json_content = json.load(open('/data/zhuoming/detection/coco/annotations/instances_train2017.json'))
 
-num_img = 100
+num_img = 10
 img_list = json_content['images']
 random.shuffle(img_list)
 
-selected_img = img_list[:100]
+selected_img = img_list[:num_img]
 img_id_dict = {ele['id']:1 for ele in selected_img}
 
 selected_annotation = []
@@ -22,47 +21,46 @@ for anno in json_content['annotations']:
     if img_id in img_id_dict:
         selected_annotation.append(anno)
 
-['info', 'licenses', 'images', 'annotations', 'categories']
-
+#['info', 'licenses', 'images', 'annotations', 'categories']
 result_json = {'info': json_content['info'], 'licenses':json_content['licenses'], 
  'categories':json_content['categories']}
 
 result_json['images'] = selected_img
 result_json['annotations'] = selected_annotation
 
-file = open('/data2/lwll/zhuoming/detection/coco/annotations/train_100imgs.json', 'w')
+file = open('/data/zhuoming/detection/coco/annotations/train_' + str(num_img) + 'imgs.json', 'w')
 file.write(json.dumps(result_json))
 file.close()
 
-# for generate the dataset for specifi image
-json_content = json.load(open('/data2/lwll/zhuoming/detection/coco/annotations/train_100imgs.json'))
+##### for generate the dataset for specifi image
+# json_content = json.load(open('/data2/lwll/zhuoming/detection/coco/annotations/train_100imgs.json'))
 
-#num_img = 100
-img_list = json_content['images']
-#random.shuffle(img_list)
+# #num_img = 100
+# img_list = json_content['images']
+# #random.shuffle(img_list)
 
-selected_img = [ele for ele in img_list if ele['id'] == 2985]
-if img_list[0]['id'] != 2985:
-    selected_img.append(img_list[0])
-else:
-    selected_img.append(img_list[1])
+# selected_img = [ele for ele in img_list if ele['id'] == 2985]
+# if img_list[0]['id'] != 2985:
+#     selected_img.append(img_list[0])
+# else:
+#     selected_img.append(img_list[1])
 
-img_id_dict = {ele['id']: 1 for ele in selected_img}
+# img_id_dict = {ele['id']: 1 for ele in selected_img}
 
-selected_annotation = []
-for anno in json_content['annotations']:
-    img_id = anno['image_id']
-    if img_id in img_id_dict:
-        selected_annotation.append(anno)
+# selected_annotation = []
+# for anno in json_content['annotations']:
+#     img_id = anno['image_id']
+#     if img_id in img_id_dict:
+#         selected_annotation.append(anno)
 
-['info', 'licenses', 'images', 'annotations', 'categories']
+# ['info', 'licenses', 'images', 'annotations', 'categories']
 
-result_json = {'info': json_content['info'], 'licenses':json_content['licenses'], 
- 'categories':json_content['categories']}
+# result_json = {'info': json_content['info'], 'licenses':json_content['licenses'], 
+#  'categories':json_content['categories']}
 
-result_json['images'] = selected_img
-result_json['annotations'] = selected_annotation
+# result_json['images'] = selected_img
+# result_json['annotations'] = selected_annotation
 
-file = open('/data2/lwll/zhuoming/detection/coco/annotations/temp.json', 'w')
-file.write(json.dumps(result_json))
-file.close()
+# file = open('/data2/lwll/zhuoming/detection/coco/annotations/temp.json', 'w')
+# file.write(json.dumps(result_json))
+# file.close()
