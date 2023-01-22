d283f018f15ac40a319eeb0809936a51c11f6045
dragonlzm <dragonlzm123@gmail.com>
2023-01-21 16:13:27 -0800

    update for the config

--- download_and_visualize_coco_most_matched_for_novel.py
+++ download_and_visualize_coco_most_matched_for_novel.py
@@ -13,7 +13,8 @@ iou_calculator = BboxOverlaps2D()
 
 # load the gt bboxes
 #gt_content = json.load(open('/data/zhuoming/detection/coco/annotations/instances_train2017_except_48base_only.json'))
-gt_content = json.load(open('/data/zhuoming/detection/coco/annotations/instances_train2017.json'))
+#gt_content = json.load(open('/data/zhuoming/detection/coco/annotations/instances_train2017.json'))
+gt_content = json.load(open('/data/zhuoming/detection/coco/annotations/instances_val2017.json'))
 
 base_names = ('person', 'bicycle', 'car', 'motorcycle', 'train', 
             'truck', 'boat', 'bench', 'bird', 'horse', 'sheep', 
@@ -61,8 +62,10 @@ for info in gt_content['images']:
     from_image_id_to_image_info[image_id] = info
 
 # load the proposal and print the image
-save_root = '/home/zhuoming/coco_visualization_most_matched'
-proposal_path_root = '/data/zhuoming/detection/coco/clip_proposal_feat/base48_finetuned_base_filtered/random'
+#save_root = '/home/zhuoming/coco_visualization_most_matched'
+save_root = '/home/zhuoming/coco_visualization_vit_most_matched'
+#proposal_path_root = '/data/zhuoming/detection/coco/clip_proposal_feat/base48_finetuned_base_filtered/random'
+proposal_path_root = '/home/zhuoming/detectron_proposal1'
 
 count = 0
 for i, image_id in enumerate(from_image_id_to_annotation):
@@ -99,14 +102,16 @@ for i, image_id in enumerate(from_image_id_to_annotation):
         rect = patches.Rectangle((box[0], box[1]),box[2],box[3],linewidth=1,edgecolor='b',facecolor='none')
         ax.add_patch(rect)
     # load the proposal 
-    pregenerate_prop_path = os.path.join(proposal_path_root, '.'.join(file_name.split('.')[:-1]) + '.json')
+    #pregenerate_prop_path = os.path.join(proposal_path_root, '.'.join(file_name.split('.')[:-1]) + '.json')
+    pregenerate_prop_path = os.path.join(proposal_path_root, (file_name + '_final_pred.json'))
     pregenerated_bbox = json.load(open(pregenerate_prop_path))
-    all_proposals = torch.tensor(pregenerated_bbox['bbox'])
+    #all_proposals = torch.tensor(pregenerated_bbox['bbox'])
+    all_proposals = torch.tensor(pregenerated_bbox['box'])
     ## scale the bbox back to the original size
-    img_metas = pregenerated_bbox['img_metas']
-    pre_extract_scale_factor = np.array(img_metas['scale_factor']+[1,1]).astype(np.float32)
+    #img_metas = pregenerated_bbox['img_metas']
+    #pre_extract_scale_factor = np.array(img_metas['scale_factor']+[1,1]).astype(np.float32)
     # all_proposals: [562.7451782226562, 133.49032592773438, 653.2548217773438, 314.5096740722656, 0.9763965010643005, 461.0]
-    all_proposals = all_proposals / pre_extract_scale_factor
+    #all_proposals = all_proposals / pre_extract_scale_factor
     
     # find the matched bbox for gt bbox "novel"
     if novel_gt_bboxes.shape[0] != 0:
@@ -119,16 +124,27 @@ for i, image_id in enumerate(from_image_id_to_annotation):
                                 novel_bbox[1] + novel_bbox[3]]])
             #print('xyxy_gt', xyxy_gt)
             real_iou = iou_calculator(xyxy_gt, all_proposals[:, :4])
-            iou_over_05 = (real_iou >= 0.1)
-            remain_proposal = all_proposals[iou_over_05.squeeze(dim=0)]
-            for box in remain_proposal:
-                cate = box[-1]
-                if cate == novel_bbox_cate_id:
-                    color = 'r'
-                else:
-                    color = 'yellow'
-                rect = patches.Rectangle((box[0], box[1]),box[2]-box[0],box[3]-box[1],linewidth=1,edgecolor=color,facecolor='none')
-                ax.add_patch(rect)    
+            # iou_over_05 = (real_iou >= 0.1)
+            # remain_proposal = all_proposals[iou_over_05.squeeze(dim=0)]
+            # for box in remain_proposal:
+            #     cate = box[-1]
+            #     if cate == novel_bbox_cate_id:
+            #         color = 'r'
+            #     else:
+            #         color = 'yellow'
+            #     rect = patches.Rectangle((box[0], box[1]),box[2]-box[0],box[3]-box[1],linewidth=1,edgecolor=color,facecolor='none')
+            #     ax.add_patch(rect)
+            iou_idx_over_zero = (real_iou > 0)
+            if torch.sum(iou_idx_over_zero) == 0:
+                continue
+            else:
+                value, idx = torch.max(real_iou, dim=-1)
+                if value < 0.5:
+                    continue
+                remain_proposal = all_proposals[idx].squeeze(dim=0)
+                rect = patches.Rectangle((remain_proposal[0], remain_proposal[1]),remain_proposal[2]-remain_proposal[0],remain_proposal[3]-remain_proposal[1],linewidth=1,edgecolor='r',facecolor='none')
+                ax.add_patch(rect)   
+
 
     print_path = os.path.join(save_root, 'printed')
     if not os.path.exists(print_path):
