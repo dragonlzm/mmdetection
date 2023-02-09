5c784e3d9d54a07f731b13c134bd9e677bf8e129
dragonlzm <dragonlzm123@gmail.com>
2023-02-09 10:30:22 -0800

    update for the script

--- my_tools_script/proposal_experiments/vote_the_pixel_min_area.py
+++ my_tools_script/proposal_experiments/vote_the_pixel_min_area.py
@@ -25,18 +25,13 @@ def get_points_single(featmap_size,
         
     return points
 
-def get_target_single(gt_bboxes, gt_labels, points, num_classes=65):
+def _get_target_single(gt_bboxes, gt_labels, points, num_classes=65):
     """Compute regression and classification targets for a single image."""
     num_points = points.size(0)
     num_gts = gt_labels.size(0)
     if num_gts == 0:
         return gt_labels.new_full((num_points,), num_classes), \
                 gt_bboxes.new_zeros((num_points, 4))
-    areas = (gt_bboxes[:, 2] - gt_bboxes[:, 0]) * (
-        gt_bboxes[:, 3] - gt_bboxes[:, 1])
-    # TODO: figure out why these two are different
-    # areas = areas[None].expand(num_points, num_gts)
-    areas = areas[None].repeat(num_points, 1)
     gt_bboxes = gt_bboxes[None].expand(num_points, num_gts, 4)
     xs, ys = points[:, 0], points[:, 1]
     xs = xs[:, None].expand(num_points, num_gts)
@@ -47,17 +42,38 @@ def get_target_single(gt_bboxes, gt_labels, points, num_classes=65):
     bottom = gt_bboxes[..., 3] - ys
     bbox_targets = torch.stack((left, top, right, bottom), -1)
     inside_gt_bbox_mask = bbox_targets.min(-1)[0] > 0
+    return inside_gt_bbox_mask
+
+
+def get_target_single(gt_bboxes, gt_labels, points, num_classes=65):
+    num_points = points.size(0)
+    areas = (gt_bboxes[:, 2] - gt_bboxes[:, 0]) * (
+    gt_bboxes[:, 3] - gt_bboxes[:, 1])
+    # TODO: figure out why these two are different
+    # areas = areas[None].expand(num_points, num_gts)
+    areas = areas[None].repeat(num_points, 1)
+    
+    total_iteration = 10
+    gap_per_iter = predict_boxes.shape[0] / total_iteration
+    all_result = []
+    for i in range(total_iteration):
+        start = int(i * gap_per_iter)
+        end = int((i+1) * gap_per_iter)
+        #print('start:end', start, end)
+        _inside_gt_bbox_mask = _get_target_single(gt_bboxes[start:end], gt_labels[start:end], all_points)
+        #print(temp_assigned_label.shape)
+        all_result.append(_inside_gt_bbox_mask)
+    inside_gt_bbox_mask = torch.cat(all_result, dim=-1)
     
     areas[inside_gt_bbox_mask == 0] = INF
     min_area, min_area_inds = areas.min(dim=1)
     labels = gt_labels[min_area_inds]
-    labels[min_area == INF] = 65  # set as BG
-
-    return labels
+    labels[min_area == INF] = num_classes  # set as BG
+    return labels    
 
 
 proposal_path = "/home/zhuoming/detectron_proposal2"
-save_path = "/home/zhuoming/label_assignment/"
+save_path = "/home/zhuoming/label_assignment_min_area/"
 
 # load the gt infomation for each image
 gt_content = json.load(open('/data/zhuoming/detection/coco/annotations/instances_val2017.json'))
@@ -67,7 +83,7 @@ for info in gt_content['images']:
     from_image_id_to_image_info[image_id] = info
 
 # for each image
-for image_id in from_image_id_to_image_info:
+for i, image_id in enumerate(from_image_id_to_image_info):
     file_name = from_image_id_to_image_info[image_id]['file_name']
     # load the vitdet final prediction 
     prediction_file_name = file_name + '_final_pred.json'
@@ -87,7 +103,6 @@ for image_id in from_image_id_to_image_info:
     all_points = get_points_single((h,w), torch.float16, torch.device('cuda'))
     # assign the result
     assigned_label = get_target_single(predict_boxes, predicted_labels, all_points)
-    print(assigned_label.shape)
     
     # visualization_result = torch.cat(visualization_result, dim=0)
     visualization_result = color[assigned_label]
@@ -99,4 +114,5 @@ for image_id in from_image_id_to_image_info:
         os.makedirs(save_path)
     data = Image.fromarray(np.uint8(visualization_result.cpu()))
     data.save(os.path.join(save_path, file_name + "_label_assign.png"))
-    break
+    if i > 10:
+        break
