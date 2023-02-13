2fc50cd3738f9ccd7ec169404798d6c883a33ddf
dragonlzm <dragonlzm123@gmail.com>
2023-02-13 01:08:02 -0800

    Update generate_lvis_rpn_proposal_feat.sh

--- tools/generate_feat/generate_lvis_rpn_proposal_feat.sh
+++ tools/generate_feat/generate_lvis_rpn_proposal_feat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --partition=gpu 
-#SBATCH --gres=gpu:v100:2
+#SBATCH --gres=gpu:p100:2
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=80GB
@@ -45,12 +45,12 @@ configs/rpn/rpn_r50_fpn_1x_lvis.py \
 data/exp_res/mask_rcnn_r50_fpn_sample1e-3_mstrain_2x_lvis_v1_freq/epoch_24.pth 2 \
 --out rpn_r50_fpn_1x_lvis.pkl \
 --cfg-options data.test.ann_file=data/lvis_v1/annotations/lvis_v1_train.json data.test.img_prefix=data/lvis_v1/ \
-model.test_cfg.bbox_save_path_root=data/lvis_v1/rpn_proposal/mask_rcnn_freq/
+model.test_cfg.bbox_save_path_root=data/lvis_v1/rpn_proposal_1/mask_rcnn_freq/
 
 ## raw clip model, rpn proposal
 CHECKPOINT="data/test/cls_finetuner_clip_base_100shots_train/epoch_0.pth"
 CONFIG_FILE="configs/cls_finetuner/cls_finetuner_clip_full_lvis.py"
-BBOX_SAVE_PATH_ROOT="data/lvis_v1/rpn_proposal/mask_rcnn_freq"
+BBOX_SAVE_PATH_ROOT="data/lvis_v1/rpn_proposal_1/mask_rcnn_freq"
 FEAT_SAVE_PATH_ROOT="data/lvis_v1/rpn_proposal_feat/freq_proposal_raw_feat"
 
 bash tools/dist_test.sh \
