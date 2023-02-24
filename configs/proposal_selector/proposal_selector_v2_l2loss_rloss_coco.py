b129ce133a02ceb8a1aa9687df561cda159e1487
dragonlzm <dragonlzm123@gmail.com>
2023-02-24 00:15:09 -0800

    Update proposal_selector_v2_l2loss_rloss_coco.py

--- configs/proposal_selector/proposal_selector_v2_l2loss_rloss_coco.py
+++ configs/proposal_selector/proposal_selector_v2_l2loss_rloss_coco.py
@@ -3,3 +3,5 @@ _base_ = './proposal_selector_v2_coco.py'
 model = dict(
     loss=dict(type='MSELoss'),
     ranking_loss=dict(type='CrossEntropyLoss'))
+
+evaluation = dict(interval=3, metric='proposal_selection')
