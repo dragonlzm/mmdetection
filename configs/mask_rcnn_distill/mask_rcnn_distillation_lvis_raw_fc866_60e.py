264e8f2094b054652423415eb9731d9af32dca3d
dragonlzm <dragonlzm123@gmail.com>
2023-07-30 18:31:51 -0700

    update for configs

---
+++ configs/mask_rcnn_distill/mask_rcnn_distillation_lvis_raw_fc866_60e.py
@@ -0,0 +1,6 @@
+_base_ = './mask_rcnn_distillation_lvis_raw_fc866.py'
+
+# learning policy
+lr_config = dict(step=[50, 55])
+runner = dict(type='EpochBasedRunner', max_epochs=60)
+evaluation = dict(interval=72, metric=['bbox', 'segm'])
